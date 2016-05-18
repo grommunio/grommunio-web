d222972cb18a014c0e37884d98e6c8e91cb4d6da
Jelle van der Waa <j.vanderwaa@kopano.com>
2016-05-18 16:50:19 +0200

    Add missing plugin config files.

---
+++ plugins/gmaps/config.php
@@ -0,0 +1,6 @@
+<?php
+//by default gmaps plugin is disabled
+define('PLUGIN_GMAPS_USER_DEFAULT_ENABLE', false);
+define ('PLUGIN_GMAPS_DEFAULT_ADDRESS','Elektronicaweg 20, 2628 XG Delft, The Netherlands');
+define ('PLUGIN_GMAPS_SHOW_ROUTES', false);
+?>
---
+++ plugins/webappmanual/config.php
@@ -0,0 +1,5 @@
+<?php
+// The base URL where the User Manual for Zarafa can be found
+define('PLUGIN_WEBAPPMANUAL_URL', 'http://doc.zarafa.com/trunk/User_Manual_WebApp/en-US/html/');
+define('PLUGIN_WEBAPPMANUAL_USER_DEFAULT_ENABLE', true);
+?>
