911f356d1d4ec5eeee80d637308a42d9b671387f
Jelle van der Waa <j.vanderwaa@kopano.com>
2016-07-29 14:19:40 +0200

    Add translations update script
    Simple script which can be used to fetch new translations strings from
    the JavaScript and PHP code and update the existing po files.

---
+++ tools/README.md
@@ -0,0 +1,17 @@
+Tools directory
+===============
+
+This directory contains the required tools to build the WebApp.
+
+Updating translations
+---------------------
+
+The WebApp PHP and JavaScript translations can be updated using the
+update_translations.sh script. Execute the scripts in the root directory of
+WebApp
+
+$ ./tools/update_translations.sh
+
+The exisiting po files in server/language will be updated with the new
+translation strings from the JavaScript and PHP code. Obsolete strings are
+automatically removed.
---
+++ tools/update_translations.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+update_translations () {
+  potfile=$1
+  lang=$2
+  files=$3
+  xgettext --keyword=_ --keyword=_W --keyword=_TT -j -L $lang --keyword=dgettext:2 --keyword=ngettext:1,2 --keyword=dngettext:2,3 --keyword=pgettext:1c,2 --keyword=dpgettext:2c,3 --keyword=npgettext:1c,2,3 --keyword=dnpgettext:2c,3,4 --add-comments=TRANSLATORS --from-code utf-8 --files-from $files -o $potfile
+}
+
+POT_FILE=webapp.pot
+
+# Otherwise xgettext complains
+touch $POT_FILE
+
+# Find JavaScript translations
+find client/zarafa plugins client/extjs-mod -type f -name '*.js' ! -path "./deploy/*" -printf '%p\n' | sort > /tmp/JSFILES
+update_translations $POT_FILE JavaScript /tmp/JSFILES # gettext 0.19.3 supports javascript, buildserver runs moldy debian wheezy.
+
+# Find PHP translations
+find . -type f -name '*.php' ! -path "./test/*" ! -path "./deploy/*" -printf '%P\n'   | sort > /tmp/PHPFILES
+update_translations $POT_FILE php /tmp/PHPFILES
+
+# Merge PO files and remove obsolete strings
+for lang in server/language/*.UTF-8/LC_MESSAGES/zarafa_webapp.po; do
+  echo $lang
+  msgmerge -q $lang $POT_FILE | msgattrib --no-obsolete -o $lang.new && mv $lang.new $lang
+done
