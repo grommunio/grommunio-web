76bb6c6c7458538a4a5027d67497b3dc9a473216
Ronald Toussaint <r.toussaint@kopano.com>
2017-03-01 22:12:02 +0100

    KW-652: Overriding Ext.EventObj.setEvent to change the behavior of the metaKey Preventing default behavior for metaKey+ENTER on OSX in the htmleditor
    Ref: KW-653

--- client/extjs-mod/Ext.EventObject.js
+++ client/extjs-mod/Ext.EventObject.js
@@ -14,4 +14,31 @@
 		CLOSE_BRACKET : 221,
 		SINGLE_QUOTE : 222
 	});
+
+	var orig_setEvent = Ext.EventObject.setEvent;
+
+	/*
+	 * We override this function because we want different behavior for the
+	 * metaKey than Ext. Ext sets the ctrlKey to true when the metaKey is
+	 * pressed. On OSX we want the CMD key (metaKey) to act as the ctrlKey
+	 * and the real ctrlKey not. On Windows/Linux we don't want the metaKey to
+	 * behave like the ctrlKey.
+	 *
+	 * @param {Event} e The Browser event object
+	 *
+	 * @return {Ext.EventObject} The normalized event object
+	 */
+	Ext.EventObject.setEvent = function(e)
+	{
+		e = orig_setEvent.call(this, e);
+
+		if ( Ext.isMac && e.ctrlKey && !e.browserEvent.metaKey ){
+			e.ctrlKey = false;
+		}
+		if ( !Ext.isMac ){
+			e.ctrlKey = e.browserEvent.ctrlKey || false;
+		}
+
+		return e;
+	};
 })();
