1b7b936f9e2297810ffd1d7dfb88a31a61930728
Jelle van der Waa <j.vanderwaa@kopano.com>
2017-08-01 13:23:08 +0200

    SettingsDefaultValues: remove unused use_files_api
    Remove this old and unused setting.

--- client/zarafa/settings/data/SettingsDefaultValues.js
+++ client/zarafa/settings/data/SettingsDefaultValues.js
@@ -36,12 +36,6 @@ Zarafa.settings.data.SettingsDefaultValue = function(){
 							 */
 							'base_content_layer' : 'tabs',
 							/**
-							 * zarafa/v1/main/use_files_api
-							 * @property
-							 * @type Boolean
-							 */
-							'use_files_api' : true,
-							/**
 							 * zarafa/v1/main/confirm_close_dialog
 							 * @property
 							 * @type Boolean
