aab17b401606e55767e8c5228f414d927062d698
Jelle van der Waa <j.vanderwaa@kopano.com>
2017-08-15 16:31:27 +0200

    Retrieve store when grid is reloaded
    When the grid reloads the record loses it's store, therefore keep the
    store in a variable and find the newly loaded record in the grid.
    
    References: KW-1224

--- client/zarafa/common/Actions.js
+++ client/zarafa/common/Actions.js
@@ -921,19 +921,24 @@ Zarafa.common.Actions = {
 						case 'ask':
 						/* falls through*/
 						default:
+							const store = record.getStore();
 							// Ask if a read receipt must be send.
 							Ext.MessageBox.confirm(_('Kopano WebApp'), _('The sender of this message has asked to be notified when you read this message. Do you wish to notify the sender?'),
 								// This function will execute when user provide some inputs,
 								// So other external changes should not affect the record.
 								function(buttonClicked) {
-									this.setReadFlags(read);
-									this.addMessageAction('send_read_receipt', buttonClicked !== 'no');
-									this.save();
+									// If the mailgrid has reloaded, retrieve the newly updated record.
+									var record = this;
+									if (!record.getStore()) {
+										record = store.getById(record.id);
+									}
+									record.setReadFlags(read);
+									record.addMessageAction('send_read_receipt', buttonClicked !== 'no');
+									record.save();
 								}, record);
 							break;
-
 					}
-		  } else {
+				} else {
 				record.setReadFlags(read);
 				saveRecords.push(record);
 				}
