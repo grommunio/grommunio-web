3e12048a7a9e63a03e52a0f4c8203a56ee0e8e7f
Snehal Dangroshiya <s.dangroshiya@kopano.com>
2018-07-30 04:54:13 -0900

    KW-1191 show email address when printing an email
    - Override the prepareData function of RecordRenderer which
    formate the to,cc and bcc recipient in name <email>; formate.
    
    Reference: KW-1191

--- client/zarafa/mail/printer/MailRenderer.js
+++ client/zarafa/mail/printer/MailRenderer.js
@@ -19,12 +19,12 @@ Zarafa.mail.printer.MailRenderer = Ext.extend(Zarafa.common.printer.renderers.Re
 		html += '<b>{fullname:htmlEncode}</b>\n';
 		html += '<hr>\n';
 		html += '<table>\n';
-		html += this.addRow(_('From'), '{sender_name:htmlEncode}');
+		html += this.addRow(_('From'), '{formatted_from:htmlEncode}');
 		// # TRANSLATORS: See http://docs.sencha.com/ext-js/3-4/#!/api/Date for the meaning of these formatting instructions
 		html += this.addRow(_('Sent'), '{message_delivery_time:date("' + _("l jS F Y G:i") + '")}');
-		html += this.optionalRow(_('To'), 'display_to', '{display_to:htmlEncode}');
-		html += this.optionalRow(_('Cc'), 'display_cc', '{display_cc:htmlEncode}');
-		html += this.optionalRow(_('Bcc'), 'display_bcc', '{display_bcc:htmlEncode}');
+		html += this.optionalRow(_('To'), 'display_to', '{formatted_to:htmlEncode}');
+		html += this.optionalRow(_('Cc'), 'display_cc', '{formatted_Cc:htmlEncode}');
+		html += this.optionalRow(_('Bcc'), 'display_bcc', '{formatted_Bcc:htmlEncode}');
 		html += this.addRow(_('Subject'), '{subject:htmlEncode}');
 		html += this.addRow('', '');   // separator
 		html += this.optionalRow(_('Sensitivity'), 'sensitivity', '{sensitivity:sensitivityString}');
@@ -40,5 +40,40 @@ Zarafa.mail.printer.MailRenderer = Ext.extend(Zarafa.common.printer.renderers.Re
 		html += '</p>\n';
 		// Remove inline images when that are not downloaded.
 		return record.isExternalContentBlocked() ? html.replace(/<img[^>]*>/g,"") : html;
+	},
+
+	/**
+	 * Prepares data for any record for use in the XTemplate
+	 * @param {Zarafa.core.data.MAPIRecord} record The mapi record to print
+	 * @return {Array} Data suitable for use in the XTemplate
+	 */
+	prepareData: function(record) {
+		var data = Zarafa.mail.printer.MailRenderer.superclass.prepareData(record);
+
+		// add printer specific tags
+		data['formatted_from'] = Ext.util.Format.htmlEncode(record.get('sender_name') +' <'+ record.get('sender_email_address') +'>;');
+		var recipientStore = record.getSubStore('recipients');
+		data['formatted_to'] = "";
+		data['formatted_Cc'] = "";
+		data['formatted_Bcc'] = "";
+		recipientStore.each(function (recipient) {
+			var name = recipient.get('display_name');
+			var email = recipient.get('smtp_address');
+			var formattedInfo = Ext.util.Format.htmlEncode(name + ' <' + email +'>; ');
+			switch (recipient.get('recipient_type')) {
+				case Zarafa.core.mapi.RecipientType.MAPI_CC:
+					data['formatted_Cc'] += formattedInfo;
+					break;
+				case Zarafa.core.mapi.RecipientType.MAPI_BCC:
+					data['formatted_Bcc'] += formattedInfo;
+					break;
+				case Zarafa.core.mapi.RecipientType.MAPI_TO:
+				default:
+					data['formatted_to'] += formattedInfo;
+					break;
+			}
+		}, this);
+
+		return data;
 	}
 });
