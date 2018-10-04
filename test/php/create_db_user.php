a8d821c1ff7caee4f47e7031280b58b6117880cb
Jelle van der Waa <j.vanderwaa@kopano.com>
2018-10-04 17:02:05 +0200

    test: Remove unrequired creation of store.

--- test/php/create_db_user.php
+++ test/php/create_db_user.php
@@ -15,7 +15,6 @@ for($i = 1; $i < 6; $i++) {
 	$display_name = constant("KOPANO_USER${i}_DISPLAY_NAME");
 
 	shell_exec("kopano-admin -c $user -p $pass -e $email_address -f '$display_name'");
-	shell_exec("kopano-admin --create-store $user");
 }
 
 # Public store
