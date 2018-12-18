145fc91ba3a3c20afb806b9076090d2b2bcfab2c
Jelle van der Waa <j.vanderwaa@kopano.com>
2018-12-18 15:56:22 +0100

    server: resolve PHP 7.3 warnings about switch statements
    PHP warns about the switch statement contains a continue which probably
    should be a break.

--- server/includes/core/class.operations.php
+++ server/includes/core/class.operations.php
@@ -669,15 +669,15 @@
 					case MAPI_E_NO_ACCESS:
 						$msg = "Unable to open FINDER_ROOT for store: %s. Run kopano-search-upgrade-findroots.py to resolve the permission issue";
 						error_log(sprintf($msg, $props[PR_DISPLAY_NAME]));
-						continue;
+						break;
 					case MAPI_E_NOT_FOUND:
 						$msg = "Unable to open FINDER_ROOT for store: %s. Folder not found.";
 						error_log(sprintf($msg, $props[PR_DISPLAY_NAME]));
-						continue;
+						break;
 					default:
 						$msg = "Unable to open FINDER_ROOT for store: %s. Unknown MAPI Error %s.";
 						error_log(sprintf($msg, $props[PR_DISPLAY_NAME], get_mapi_error_name($e->getCode())));
-						continue;
+						break;
 					}
 				}
 			}
