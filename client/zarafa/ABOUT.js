42169e917b69cac2876c2635281e8c26ddc7cfdc
Ronald Toussaint <r.toussaint@kopano.com>
2019-01-22 11:47:46 +0100

    Add Tokenizer license
    Ref: KW-2948

--- client/zarafa/ABOUT.js
+++ client/zarafa/ABOUT.js
@@ -64,7 +64,7 @@ Zarafa.ABOUT = ""
 	+ "<h1>TinyMCE PowerPaste</h1>"
 
 	+ "<p>Copyright (C) 2015 Ephox Corporation</p>"
-	
+
 	+ "<p>This program is free software: you can redistribute it and/or modify "
 	+ "it under the terms of the GNU Affero General Public License as published by "
 	+ "the Free Software Foundation, either version 3 of the License, or "
@@ -76,4 +76,27 @@ Zarafa.ABOUT = ""
 	+ "GNU Affero General Public License for more details.</p>"
 
 	+ "You should have received a copy of the GNU Affero General Public License "
-	+ "along with this program. If not, see <a href=\"https://www.gnu.org/licenses/\" target=\"_blank\">https://www.gnu.org/licenses/</a>.</p>";
+	+ "along with this program. If not, see <a href=\"https://www.gnu.org/licenses/\" target=\"_blank\">https://www.gnu.org/licenses/</a>.</p>"
+
+	+ "<h1>Tokenizr -- String Tokenization Library</h1>"
+
+	+ "<p>Copyright (c) 2015-2018 Ralf S. Engelschall  <a href=\"mailto:rse@engelschall.com\">rse@engelschall.com</a></p>"
+
+	+ "Permission is hereby granted, free of charge, to any person obtaining"
+	+ "a copy of this software and associated documentation files (the"
+	+ "\"Software\"), to deal in the Software without restriction, including"
+	+ "without limitation the rights to use, copy, modify, merge, publish,"
+	+ "distribute, sublicense, and/or sell copies of the Software, and to"
+	+ "permit persons to whom the Software is furnished to do so, subject to"
+	+ "the following conditions:</p>"
+
+	+ "<p>The above copyright notice and this permission notice shall be included"
+	+ "in all copies or substantial portions of the Software.</p>"
+
+	+ "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND,"
+	+ "EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF"
+	+ "MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT."
+	+ "IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY"
+	+ "CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,"
+	+ "TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE"
+	+ "SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>";
