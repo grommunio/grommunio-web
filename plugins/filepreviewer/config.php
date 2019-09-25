5c6e4e1f8cf31e8919f33ca5f4dbc083d8c2047b
steep-system <yangsongyi@ee-post.com>
2019-09-25 20:30:30 +0800

    first upload of source codes

---
+++ plugins/filepreviewer/config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Enable this plugin by default for all users. (default: true)
+ */
+define('PLUGIN_FILEPREVIEWER_USER_DEFAULT_ENABLE', true);
+
+/**
+ * #########################
+ *     ADVANCED SECTION
+ * #########################
+ */
+
+/**
+ * Default zoom mode for PDF documents. (default: page-width) [Allowed: "auto", "page-actual", "page-width"]
+ */
+define('PLUGIN_FILEPREVIEWER_PDF_DEFAULT_ZOOM', "page-width");
+/**
+ * Default zoom mode for ODF documents. (default: page-width) [Allowed: "auto", "page-actual", "page-width"]
+ */
+define('PLUGIN_FILEPREVIEWER_ODF_DEFAULT_ZOOM', "auto");
---
+++ plugins/passwd/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// Enable the passwd plugin for all clients
+define('PLUGIN_PASSWD_USER_DEFAULT_ENABLE', true);
+
+// Enable the passwd plugin for all clients
+define('PLUGIN_PASSWD_STRICT_CHECK_ENABLE', true);
+
+
+?>
