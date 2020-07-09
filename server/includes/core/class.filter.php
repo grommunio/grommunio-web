e4071a35986ef48174a208807ce558b475954be4
Martijn Alberts <m.alberts@kopano.com>
2020-07-09 16:38:40 +0200

    Enable DOMPurify by default

--- server/includes/core/class.filter.php
+++ server/includes/core/class.filter.php
@@ -8,6 +8,18 @@
 	 */
 	class filter
 	{
+
+		/**
+		 * A boolean value set true to Filter html content forcefuly default is false.
+		 * @var bool
+		 */
+		private $forceFilterHTML;
+
+		function __construct($forceFilterHTML = false)
+		{
+			$this->forceFilterHTML = $forceFilterHTML;
+		}	
+
 		/**
 		 * Create script-safe HTML from raw HTML
 		 *
@@ -19,7 +31,7 @@
 			// Save all "<" symbols
 			$html = preg_replace("/<(?=[^a-zA-Z\/\!\?\%])/", "&lt;", $html);
 
-			if(!DISABLE_HTMLBODY_FILTER && !ENABLE_DOMPURIFY_FILTER){
+			if(!DISABLE_HTMLBODY_FILTER && (!ENABLE_DOMPURIFY_FILTER || $this->forceFilterHTML)) {
 				// Filter '<script>'
 				$html = magicHTML($html);
 			}
