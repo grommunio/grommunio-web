8372ebc4512b9bd3e08ef277053156017a91d124
Mehul Vasava <m.vasava@kopano.com>
2020-09-10 07:42:32 +0200

    KW-3527-dompurify-strips-network-links-and-titles
        - Add a regex to allow network urls starting with directory name (i.e C:)
    
    Reference KW-3527

--- client/zarafa/Zarafa.js
+++ client/zarafa/Zarafa.js
@@ -234,7 +234,10 @@ Ext.apply(Zarafa, {
 		// Set up DOMPurify
 		DOMPurify.setConfig({
 			FORBID_TAGS: ['iframe', 'webview'],
-			WHOLE_DOCUMENT: false
+			WHOLE_DOCUMENT: false,
+			// Default regEx of DOMPurify for uri does not allow some protocols like file, smb, etc.
+			// So we need to whitelist them by this new regEx.
+			ALLOWED_URI_REGEXP: Object.seal(/^(?:(?:(?:f|ht)tps?|mailto|tel|callto|cid|xmpp|smb|file):|[^a-z]|[a-z]:|[a-z+.\-]+(?:[^a-z+.\-:]|$))/i)
 		});
 
 		DOMPurify.addHook('afterSanitizeAttributes', function(node) {
