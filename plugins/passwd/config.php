d6470ab00cb76e16f2ba087a340b246a53450fa2
Stefan Akie <stefan.akie@grammm.com>
2021-09-08 14:15:29 +0000

    Update docs url

--- plugins/passwd/config.php
+++ plugins/passwd/config.php
@@ -9,7 +9,7 @@ define('PLUGIN_PASSWD_USER_DEFAULT_ENABLE', false);
 // Enable the passwd plugin for all clients
 define('PLUGIN_PASSWD_STRICT_CHECK_ENABLE', true);
 
-// The grammm admin API passwd endpoint
+// The grommunio admin API passwd endpoint
 define('PLUGIN_PASSWD_ADMIN_API_ENDPOINT', 'http://[::1]:8080/api/v1/passwd');
 
 ?>
--- plugins/webappmanual/config.php
+++ plugins/webappmanual/config.php
@@ -1,5 +1,5 @@
 <?php
 // The base URL where the User Manual for WebApp can be found
-define('PLUGIN_WEBAPPMANUAL_URL', 'https://docs.grammm.com/web');
+define('PLUGIN_WEBAPPMANUAL_URL', 'https://docs.grommunio.com/web');
 define('PLUGIN_WEBAPPMANUAL_USER_DEFAULT_ENABLE', true);
 ?>
