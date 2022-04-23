43d3497b95e25a7cbde7b1353b5bbe7fe841d2cc
Mike Williams <mike.williams@grommunio.com>
2022-04-23 17:21:03 +0200

    dev: de-duplicate licensing information cluttered throughout repository

--- client/zarafa/ABOUT.js
+++ client/zarafa/ABOUT.js
@@ -9,7 +9,7 @@ Ext.namespace('Zarafa');
 
 Zarafa.ABOUT = ""
 	+ "<style>div.atxt h1{font-weight:inherit;font-style:italic;}</style><div class=\"atxt\">"
-	+ "<p>Copyright 2020-2021 grommunio GmbH</p>"
+	+ "<p>Copyright (C) 2020 - 2022 grommunio GmbH</p>"
 
 	+ "<p>This program is free software: you can redistribute it and/or modify "
 	+ "it under the terms of the GNU Affero General Public License as "
@@ -26,28 +26,72 @@ Zarafa.ABOUT = ""
 
 	+ "<hr />"
 
-	+ "<p>grommunio-web contains the following third-party components:</p>"
+	+ "<p>grommunio Web contains the following components:</p>"
 
-	+ "<h1>Ext JS</h1>"
+	+ "<h1>grommunio Web</h1>"
 
-	+ "<p>Copyright (c) 2011-2013 Sencha Inc</p>"
+	+ "<p>Copyright (C) 2020 - 2022 grommunio GmbH<br>"
+	+ "Copyright (C) 2016 Kopano and its licensors<br>"
+	+ "Copyright (C) 2005 - 2016 Zarafa B.V. and its licensors</p>"
 
-	+ "<p>Ext JS is free software: you can redistribute it and/or modify "
-	+ "it under the terms of the GNU General Public License version 3.0 as "
-	+ "published by the Free Software Foundation."
-	+ "<a href=\"https://www.gnu.org/copyleft/gpl.html\" target=\"_blank\">https://www.gnu.org/copyleft/gpl.html</a></p>"
+	+ "<h1>grommunio Web File Previewer Plugin</h1>"
 
-	+ "<h1>kopano-webapp</h1>"
+	+ "<p>Copyright (C) 2020 - 2022 grommunio GmbH<br>"
+	+ "Copyright (C) 2016 Kopano and its licensors<br>"
+	+ "Copyright (C) 2016 Zarafa B.V. and its licensors</p>"
 
-	+ "<p>Copyright (C) 2005 - 2016 Zarafa B.V. and its licensors<br>"
-	+ "Copyright (C) 2016 Kopano B.V. and its licensors</p>"
+	+ "<h1>grommunio Web Desktop Notifications Plugin</h1>"
 
-	+ "<p>WebApp is free software: you can redistribute it and/or modify "
+	+ "<p>Copyright (C) 2020 - 2022 grommunio GmbH<br>"
+	+ "Copyright (C) 2016 Kopano and its licensors<br>"
+	+ "Copyright (C) 2013 Saket Patel</p>"
+
+	+ "<h1>grommunio Web Meet Plugin</h1>"
+
+	+ "<p>Copyright (C) 2020 - 2022 grommunio GmbH<br>"
+	+ "Copyright (C) 2020 Siedl Networks<br>"
+	+ "Copyright (C) 2017 Kopano and its licensors</p>"
+
+	+ "<h1>grommunio Web Password Plugin</h1>"
+
+	+ "<p>Copyright (C) 2020 - 2022 grommunio GmbH<br>"
+	+ "Copyright (C) 2017 Kopano and its licensors<br>"
+	+ "Copyright (C) 2013 - 2017 Saket Patel</p>"
+
+	+ "<h1>grommunio Web Device Management Plugin</h1>"
+
+	+ "<p>Copyright (C) 2020 - 2022 grommunio GmbH<br>"
+	+ "Copyright (C) 2016 Kopano and its licensors<br>"
+	+ "Copyright (C) 2005 - 2016 Zarafa B.V. and its licensors</p>"
+
+	+ "<h1>grommunio Web Chat Plugin</h1>"
+
+	+ "<p>Copyright (C) 2020 - 2022 grommunio GmbH<br>"
+	+ "Copyright (C) 2017 Kopano and its licensors</p>"
+
+	+ "<h1>grommunio Web Maps Plugin</h1>"
+
+	+ "<p>Copyright (C) 2020 - 2022 grommunio GmbH<br>"
+	+ "Copyright (C) 2016 Kopano and its licensors<br>"
+	+ "Copyright (C) 2005 - 2016 Zarafa B.V. and its licensors</p>"
+
+	+ "<h1>grommunio Web Files & Backend Plugin</h1>"
+
+	+ "<p>Copyright (C) 2020 - 2022 grommunio GmbH<br>"
+	+ "Copyright (C) 2016 Kopano and its licensors<br>"
+	+ "Copyright (C) 2016 Zarafa B.V. and its licensors</p>"
+
+	+ "<h1>grommunio Web Intranet Plugin</h1>"
+
+	+ "<p>Copyright (C) 2020 - 2022 grommunio GmbH<br>"
+	+ "Copyright (C) 2017 Kopano and its licensors</p>"
+
+	+ "<p>This program is free software: you can redistribute it and/or modify "
 	+ "it under the terms of the GNU Affero General Public License as "
 	+ "published by the Free Software Foundation, either version 3 of the "
 	+ "License, or (at your option) any later version.</p>"
 
-	+ "<p>WebApp is distributed in the hope that it will be useful, "
+	+ "<p>This program is distributed in the hope that it will be useful, "
 	+ "but WITHOUT ANY WARRANTY; without even the implied warranty of "
 	+ "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
 	+ "GNU Affero General Public License for more details.</p>"
@@ -55,6 +99,98 @@ Zarafa.ABOUT = ""
 	+ "<p>You should have received a copy of the GNU Affero General Public License "
 	+ "along with this program. "
 	+ "If not, see <a href=\"https://www.gnu.org/licenses/\" target=\"_blank\">https://www.gnu.org/licenses/</a>.</p>"
+
+
+	+ "<h1>Ext JS</h1>"
+
+	+ "<p>Copyright (C) 2011-2013 Sencha Inc</p>"
+
+	+ "<p>Ext JS is free software: you can redistribute it and/or modify "
+	+ "it under the terms of the GNU General Public License version 3.0 as "
+	+ "published by the Free Software Foundation."
+	+ "<a href=\"https://www.gnu.org/copyleft/gpl.html\" target=\"_blank\">https://www.gnu.org/copyleft/gpl.html</a></p>"
+
+
+	+ "<h1>Ext.ux.form.Metaform</h1>"
+
+	+ "<p>Copyright (C) 2008 Ing. Jozef Sakáloša.</p>"
+
+	+ "<p>Ext.ux.form.MetaForm is licensed under the terms of the Open Source LGPL 3.0 "
+	+ "license. Commercial use is permitted to the extent that the code/component(s)"
+	+ "do NOT become part of another Open Source or Commercially licensed development"
+	+ "library or toolkit without explicit permission."
+	+ "<a href=\"https://www.gnu.org/copyleft/lgpl.html\" target=\"_blank\">https://www.gnu.org/copyleft/lgpl.html</a></p>"
+
+
+	+ "<h1>Phpfastcache</h1>"
+
+	+ "<p>Copyright (C) 2016 Phpfastcache</p>"
+
+	+ "<p>Permission is hereby granted, free of charge, to any person obtaining a copy of "
+	+ "this software and associated documentation files (the Software), to deal in the "
+	+ "Software without restriction, including without limitation the rights to use, "
+	+ "copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the" 
+	+ "Software, and to permit persons to whom the Software is furnished to do so, "
+	+ "subject to the following conditions:</p>"
+
+	+ "<p>The above copyright notice and this permission notice shall be included in all "
+	+ "copies or substantial portions of the Software.</p>"
+
+	+ "<p>THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR "
+	+ "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, "
+	+ "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE "
+	+ "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER "
+	+ "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, "
+	+ "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE "
+	+ "SOFTWARE.</p>"
+
+
+	+ "<h1>Sabre-dav</h1>"
+
+	+ "<p>Copyright (C) 2007-2016 fruux GmbH (https://fruux.com/)</p>"
+	+ "<p>Sabre-dav is licensed under the BSD3 license.</p>"
+
+	+ "<p>Permission is hereby granted, free of charge, to any person obtaining a copy "
+	+ "of this software and associated documentation files (the \"Software\"), to deal "
+	+ "in the Software without restriction, including without limitation the rights "
+	+ "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell "
+	+ "copies of the Software, and to permit persons to whom the Software is "
+	+ "furnished to do so, subject to the following conditions:</p>"
+
+	+ "<p>The above copyright notice and this permission notice shall be included in "
+	+ "all copies or substantial portions of the Software.</p>"
+
+	+ "<p>THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR "
+	+ "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, "
+	+ "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE "
+	+ "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER "
+	+ "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, "
+	+ "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN "
+	+ "THE SOFTWARE.</p>"
+
+	+ "<p>Redistribution and use in source and binary forms, with or without "
+	+ "modification, are permitted provided that the following conditions are met:</p>"
+
+	+ "<p><ul><li>Redistributions of source code must retain the above copyright notice, this "
+	+ "list of conditions and the following disclaimer.</li>"
+	+ "<li>Redistributions in binary form must reproduce the above copyright notice, "
+	+ "this list of conditions and the following disclaimer in the documentation "
+	+ "and/or other materials provided with the distribution.</li>"
+	+ "<li>Neither the name of SabreDAV nor the names of its contributors may be used "
+	+ "to endorse or promote products derived from this software without specific "
+	+ "prior written permission.</li></p>"
+
+	+ "<p>THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND "
+	+ "ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED "
+	+ "WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE "
+	+ "DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR "
+	+ "ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES "
+	+ "(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; "
+	+ "LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON "
+	+ "ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT "
+	+ "(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS "
+	+ "SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.</p>"
+
 
 	+ "<h1>Printer rendering</h1>"
 
@@ -96,9 +232,10 @@ Zarafa.ABOUT = ""
 	+ "License along with this library; "
 	+ "if not, see <a href=\"https://www.gnu.org/licenses/\" target=\"_blank\">https://www.gnu.org/licenses/</a>.</p>"
 
+
 	+ "<h1>TinyMCE PowerPaste</h1>"
 
-	+ "<p>Copyright (C) 2015 Ephox Corporation</p>"
+	+ "<p>Copyright (C) 2010-2015 Ephox Corporation</p>"
 
 	+ "<p>This program is free software: you can redistribute it and/or modify "
 	+ "it under the terms of the GNU Affero General Public License as published by "
@@ -113,9 +250,10 @@ Zarafa.ABOUT = ""
 	+ "You should have received a copy of the GNU Affero General Public License "
 	+ "along with this program. If not, see <a href=\"https://www.gnu.org/licenses/\" target=\"_blank\">https://www.gnu.org/licenses/</a>.</p>"
 
-	+ "<h1>Tokenizr -- String Tokenization Library</h1>"
 
-	+ "<p>Copyright (c) 2015-2018 Ralf S. Engelschall <a href=\"mailto:rse@engelschall.com\">rse@engelschall.com</a></p>"
+	+ "<h1>Tokenizr</h1>"
+
+	+ "<p>Copyright (C) 2015-2018 Ralf S. Engelschall <a href=\"mailto:rse@engelschall.com\">rse@engelschall.com</a></p>"
 
 	+ "Permission is hereby granted, free of charge, to any person obtaining"
 	+ "a copy of this software and associated documentation files (the"
@@ -134,7 +272,19 @@ Zarafa.ABOUT = ""
 	+ "IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY"
 	+ "CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,"
 	+ "TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE"
-	+ "SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p></div>"
+	+ "SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>"
+
 
 	+ "<h1>DOMPurify</h1>"
-	+ "<p>Copyright 2015 Mario Heiderich. Licensed under the Apache License, version 2.0.</p>";
+
+	+ "<p>Copyright 2015 Mario Heiderich.</p><p>Licensed under the Apache License, version 2.0.</p>";
+
+
+	+ "<h1>ViewerJS</h1>"
+
+	+ "<p>Copyright (C) 2015 viewerjs.org</p>"
+
+
+	+ "<h1>PDF.js</h1>"
+
+	+ "<p>Copyright (C) Mozilla and individual contributors</p></div>";
