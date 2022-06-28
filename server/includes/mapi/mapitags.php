9f91a458ffb290e3d3c479703aba20c81e851b72
Andreas Lang <andreas.lang@grommunio.com>
2022-06-28 19:07:26 +0000

    php_mapi: add missing tags
    References: Issue #98

--- server/includes/mapi/mapitags.php
+++ server/includes/mapi/mapitags.php
@@ -5,6 +5,13 @@ mapi_load_mapidefs();
 /* Search folder properties */
 define('PR_EC_SUGGESTION', mapi_prop_tag(PT_TSTRING, 0x6707));
 
+// custom properties to hold size units
+define('PR_RULE_ATLEAST_MESSAGE_SIZEUNIT', mapi_prop_tag(PT_STRING8, 0x6715));
+define('PR_RULE_ATMOST_MESSAGE_SIZEUNIT', mapi_prop_tag(PT_STRING8, 0x6717));
+// custom properties which holds size units for exceptions
+define('PR_RULE_EXCEPTION_ATLEAST_MESSAGE_SIZEUNIT', mapi_prop_tag(PT_STRING8, 0x6718));
+define('PR_RULE_EXCEPTION_ATMOST_MESSAGE_SIZEUNIT', mapi_prop_tag(PT_STRING8, 0x6719));
+
 /* Server version */
 define('PR_EC_SERVER_VERSION', mapi_prop_tag(PT_STRING8, 0x6716));
 
