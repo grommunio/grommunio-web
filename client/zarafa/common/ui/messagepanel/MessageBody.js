3f03af8935a6a63584dfc36c39ac57c388aa9c3f
Jan Engelhardt <jengelh@inai.de>
2024-02-13 15:09:47 +0100

    css: restore monospace font
    Partial revert of grommunio-web-3.4-10-g736b3919.

--- client/zarafa/common/ui/messagepanel/MessageBody.js
+++ client/zarafa/common/ui/messagepanel/MessageBody.js
@@ -407,7 +407,7 @@ Zarafa.common.ui.messagepanel.MessageBody = Ext.extend(Ext.Container, {
 			// Make the blockquote element not use the default right margin of 40px
 			'blockquote { margin-right: 0px; }' +
 			// Make text in pre tags wrapped if too long for a line
-			"pre { white-space: pre-wrap; margin: 0; font-family:'Roboto', sans-serif; }" +
+			"pre { white-space: pre-wrap; margin: 0; font-family: 'Fira Mono', monospace; }" +
 
 			// Scale images
 			'p > span > img, div > img, p > img { max-width: 100%; height: auto !important; }' +
