ab1b16625e97d15c65dea4f58be94383bfd78809
Mike Williams <mike.williams@grommunio.com>
2024-09-10 23:51:19 +0200

    DOMpurify: xlink:show is bound to spec, do not remove
    This commit reverts recommended node removal as recommended by bsi,
    which is invalid, as xlink:show is mandatory as per XLink 1.1 and any
    non-sidechannel content wouldn't survive the parser stage.

--- client/zarafa/Zarafa.js
+++ client/zarafa/Zarafa.js
@@ -256,9 +256,6 @@ Ext.apply(Zarafa, {
 				// set non-HTML/MathML links to xlink:show=new
 				node.setAttribute('xlink:show', 'new');
 			}
-			if ((node.hasAttribute('xlink:href') && !node.getAttribute('xlink:href').match(/^#/))) {
-				node.remove()
-			}
 		});
 
 	},
