bc49c4ae5db3d2a72e1cefe53edc13577f953814
Mike Williams <mike.williams@grommunio.com>
2024-11-05 20:03:32 +0100

    tinymce: upgrade plugins and debug files to 7.4.1

--- client/zarafa/common/ui/HtmlEditor.js
+++ client/zarafa/common/ui/HtmlEditor.js
@@ -81,7 +81,7 @@ Zarafa.common.ui.HtmlEditor = Ext.extend(Ext.ux.form.TinyMCETextArea, {
 		var fontFamilies = Zarafa.common.ui.htmleditor.Fonts.getFontFamilies();
 
 		var baseUrl = container.getServerConfig().getBaseUrl();
-		const cacheBuster = "7.4.1.61";
+		const cacheBuster = "7.4.1.144";
 
 		var themeIsDark = container.getSettingsModel().get("zarafa/v1/main/active_theme") === "dark";
 
