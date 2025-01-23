d958af437df2266d0380e5bc463566b94d5d165f
Jan Engelhardt <jengelh@inai.de>
2025-01-23 17:03:44 +0100

    server: reduce indent in download_message.php

--- server/includes/download_message.php
+++ server/includes/download_message.php
@@ -17,29 +17,29 @@ class DownloadMessage extends DownloadBase {
 	 * It also sends the eml file to the client.
 	 */
 	public function downloadMessageAsFile() {
-		if ($this->message && $this->store) {
-			// get message properties.
-			$messageProps = mapi_getprops($this->message, [PR_SUBJECT, PR_MESSAGE_CLASS]);
+		if (!$this->message || !$this->store)
+			return;
+		// get message properties.
+		$messageProps = mapi_getprops($this->message, [PR_SUBJECT, PR_MESSAGE_CLASS]);
 
-			$stream = $this->getEmlStream($messageProps);
+		$stream = $this->getEmlStream($messageProps);
 
-			$filename = (!empty($messageProps[PR_SUBJECT])) ? $messageProps[PR_SUBJECT] : _('Untitled');
-			$filename .= '.eml';
+		$filename = (!empty($messageProps[PR_SUBJECT])) ? $messageProps[PR_SUBJECT] : _('Untitled');
+		$filename .= '.eml';
 
-			// Set the file length
-			$stat = mapi_stream_stat($stream);
+		// Set the file length
+		$stat = mapi_stream_stat($stream);
 
-			$this->setNecessaryHeaders($filename, $stat['cb']);
+		$this->setNecessaryHeaders($filename, $stat['cb']);
 
-			// Read whole message and echo it.
-			for ($i = 0; $i < $stat['cb']; $i += BLOCK_SIZE) {
-				// Print stream
-				echo mapi_stream_read($stream, BLOCK_SIZE);
+		// Read whole message and echo it.
+		for ($i = 0; $i < $stat['cb']; $i += BLOCK_SIZE) {
+			// Print stream
+			echo mapi_stream_read($stream, BLOCK_SIZE);
 
-				// Need to discard the buffer contents to prevent memory
-				// exhaustion while echoing large content.
-				ob_flush();
-			}
+			// Need to discard the buffer contents to prevent memory
+			// exhaustion while echoing large content.
+			ob_flush();
 		}
 	}
 
@@ -49,71 +49,71 @@ class DownloadMessage extends DownloadBase {
 	 * Send ZIP to the client if all the requested eml files included successfully into the same.
 	 */
 	public function downloadMessageAsZipFile() {
-		if ($this->store) {
-			// Generate random ZIP file name at default temporary path of PHP
-			$randomZipName = tempnam(sys_get_temp_dir(), 'zip');
+		if (!$this->store)
+			return;
+		// Generate random ZIP file name at default temporary path of PHP
+		$randomZipName = tempnam(sys_get_temp_dir(), 'zip');
 
-			// Create an open zip archive.
-			$zip = new ZipArchive();
-			$result = $zip->open($randomZipName, ZipArchive::CREATE);
+		// Create an open zip archive.
+		$zip = new ZipArchive();
+		$result = $zip->open($randomZipName, ZipArchive::CREATE);
 
-			if ($result === true) {
-				for ($index = 0, $count = count($this->entryIds); $index < $count; ++$index) {
-					$this->message = mapi_msgstore_openentry($this->store, hex2bin($this->entryIds[$index]));
+		if ($result === true) {
+			for ($index = 0, $count = count($this->entryIds); $index < $count; ++$index) {
+				$this->message = mapi_msgstore_openentry($this->store, hex2bin($this->entryIds[$index]));
 
-					// get message properties.
-					$messageProps = mapi_getprops($this->message, [PR_SUBJECT, PR_MESSAGE_CLASS]);
+				// get message properties.
+				$messageProps = mapi_getprops($this->message, [PR_SUBJECT, PR_MESSAGE_CLASS]);
 
-					$stream = $this->getEmlStream($messageProps);
-					$stat = mapi_stream_stat($stream);
+				$stream = $this->getEmlStream($messageProps);
+				$stat = mapi_stream_stat($stream);
 
-					// Get the stream
-					$datastring = '';
-					for ($i = 0; $i < $stat['cb']; $i += BLOCK_SIZE) {
-						$datastring .= mapi_stream_read($stream, BLOCK_SIZE);
-						// Need to discard the buffer contents to prevent memory
-						// exhaustion.
-						ob_flush();
-					}
-
-					$filename = (!empty($messageProps[PR_SUBJECT])) ? $messageProps[PR_SUBJECT] : _('Untitled');
-					$filename .= '.eml';
-
-					$filename = $this->handleDuplicateFileNames($filename);
-					// Remove slashes to prevent unwanted directories to be created in the zip file.
-					$filename = str_replace('\\', '_', $filename);
-					$filename = str_replace('/', '_', $filename);
-
-					// Add file into zip by stream
-					$zip->addFromString($filename, $datastring);
+				// Get the stream
+				$datastring = '';
+				for ($i = 0; $i < $stat['cb']; $i += BLOCK_SIZE) {
+					$datastring .= mapi_stream_read($stream, BLOCK_SIZE);
+					// Need to discard the buffer contents to prevent memory
+					// exhaustion.
+					ob_flush();
 				}
-			}
-			else {
-				$zip->close();
-				// Remove the zip file to avoid unnecessary disk-space consumption
-				unlink($randomZipName);
 
-				// Throw exception if ZIP is not created successfully
-				throw new ZarafaException(_("ZIP is not created successfully"));
-			}
+				$filename = (!empty($messageProps[PR_SUBJECT])) ? $messageProps[PR_SUBJECT] : _('Untitled');
+				$filename .= '.eml';
 
+				$filename = $this->handleDuplicateFileNames($filename);
+				// Remove slashes to prevent unwanted directories to be created in the zip file.
+				$filename = str_replace('\\', '_', $filename);
+				$filename = str_replace('/', '_', $filename);
+
+				// Add file into zip by stream
+				$zip->addFromString($filename, $datastring);
+			}
+		}
+		else {
 			$zip->close();
-
-			// Set the headers
-			header('Pragma: public');
-			header('Expires: 0'); // set expiration time
-			header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-			header('Content-Disposition: attachment; filename="' . addslashes(browserDependingHTTPHeaderEncode(_("Messages") . date(" d-m-Y") . ".zip")) . '"');
-			header('Content-Transfer-Encoding: binary');
-			header('Content-Type:  application/zip');
-			header('Content-Length: ' . filesize($randomZipName));
-
-			// Send the actual response as ZIP file
-			readfile($randomZipName);
-
 			// Remove the zip file to avoid unnecessary disk-space consumption
 			unlink($randomZipName);
+
+			// Throw exception if ZIP is not created successfully
+			throw new ZarafaException(_("ZIP is not created successfully"));
 		}
+
+		$zip->close();
+
+		// Set the headers
+		header('Pragma: public');
+		header('Expires: 0'); // set expiration time
+		header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+		header('Content-Disposition: attachment; filename="' . addslashes(browserDependingHTTPHeaderEncode(_("Messages") . date(" d-m-Y") . ".zip")) . '"');
+		header('Content-Transfer-Encoding: binary');
+		header('Content-Type:  application/zip');
+		header('Content-Length: ' . filesize($randomZipName));
+
+		// Send the actual response as ZIP file
+		readfile($randomZipName);
+
+		// Remove the zip file to avoid unnecessary disk-space consumption
+		unlink($randomZipName);
 	}
 
 	/**
