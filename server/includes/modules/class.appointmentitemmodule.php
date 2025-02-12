ed595c586cb7e60f5f75e03abd60c8f3c8a4f0c8
Andreas Lang <andreas.lang@grommunio.com>
2025-02-12 17:41:26 +0100

    Change conditions of all-day events processing
    Recalculate start and end dates for all-day events when opening them
    only if PidLidAppointmentTimeZoneDefinition* is not set and the
    timezones are not the same in case PidLidAppointmentTimeZoneDefinition
    is set.
    
    References: Issue #298

--- server/includes/modules/class.appointmentitemmodule.php
+++ server/includes/modules/class.appointmentitemmodule.php
@@ -380,13 +380,14 @@ class AppointmentItemModule extends ItemModule {
 		// If the appointment doesn't have tzdefstart property, it was probably
 		// created on a mobile device (mobile devices do not send a timezone for
 		// all-day events).
-		$tzdefstart = isset($calendaritem['props']['tzdefstart']) ?
+		$isTzdefstartSet = isset($calendaritem['props']['tzdefstart']);
+		$tzdefstart = $isTzdefstartSet ?
 			hex2bin($calendaritem['props']['tzdefstart']) :
 			mapi_ianatz_to_tzdef("Etc/UTC");
 
 		// Compare the timezone definitions of the client and the appointment.
 		// Further processing is only required if they don't match.
-		if ($GLOBALS['entryid']->compareEntryIds($this->tzdef, $tzdefstart)) {
+		if ($isTzdefstartSet && $GLOBALS['entryid']->compareEntryIds($this->tzdef, $tzdefstart)) {
 			return;
 		}
 
