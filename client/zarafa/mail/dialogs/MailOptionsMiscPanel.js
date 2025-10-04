4eb12f58756e0e9ceb27a4e3caeb54afe6662e45
Jan Engelhardt <jengelh@inai.de>
2025-10-04 17:33:29 +0200

    client: add ObjectID field to draft mail options panel

--- client/zarafa/mail/dialogs/MailOptionsMiscPanel.js
+++ client/zarafa/mail/dialogs/MailOptionsMiscPanel.js
@@ -84,4 +84,56 @@ Zarafa.mail.dialogs.MailOptionsMiscPanel = Ext.extend(Ext.form.FormPanel, {
 	}
 });
 
+/* A reduced MiscPanel for Draft messages where PR_TRANSPORT_HEADERS does not make sense */
+Zarafa.mail.dialogs.MailOptionsMsgidPanel = Ext.extend(Ext.form.FormPanel, {
+	/**
+	 * @constructor
+	 * @param {Object} config Configuration object
+	 */
+	constructor: function(config)
+	{
+		config = config || {};
+
+		config.plugins = Ext.value(config.plugins, []);
+		config.plugins.push('zarafa.recordcomponentupdaterplugin');
+
+		config = Ext.applyIf(config, {
+			xtype: 'zarafa.mailoptionsmiscpanel',
+			title: _('Properties'),
+			layout: 'form',
+			items: [{
+				xtype: 'textfield',
+				fieldLabel: _('Object ID'),
+				name: 'x_midtext',
+				readOnly: true,
+				anchor: '100%',
+				autoScroll: true,
+				border: false
+			}]
+		});
+
+		Zarafa.mail.dialogs.MailOptionsMsgidPanel.superclass.constructor.call(this, config);
+	},
+
+	update: function(record, contentReset)
+	{
+		this.record = record;
+
+		if (record)
+			this.getForm().loadRecord(record);
+		else
+			this.headersTextArea.setValue('');
+	},
+
+	/**
+	 * Update the {@link Zarafa.core.data.IPMRecord IPMRecord} with the data from the {@link Ext.Panel Panel}.
+	 * @param {Zarafa.core.data.IPMRecord} record The record which has to be updated
+	 */
+	updateRecord: function(record)
+	{
+		this.getForm().updateRecord(record);
+	}
+});
+
 Ext.reg('zarafa.mailoptionsmiscpanel', Zarafa.mail.dialogs.MailOptionsMiscPanel);
+Ext.reg('zarafa.mailoptionsmsgidpanel', Zarafa.mail.dialogs.MailOptionsMsgidPanel);
