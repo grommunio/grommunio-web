grommunio/grommunio-web

commit 2b7b06719e05ae419c5cd3542240b1f68faa1919

first upload of source codes

server/includes/core/class.state.php
@@ -1,0 +1,178 @@
+<?php
+
+/**
+ * Secondary state handling
+ *
+ * This class works exactly the same as standard PHP sessions files. We implement it here
+ * so we can have improved granularity and don't have to put everything into the session
+ * variables. Basically we have multiple session objects corresponding to multiple webclient
+ * objects on the client. This class also locks the session files (in the same way as standard
+ * PHP sessions), in order to serialize requests from the same client object.
+ *
+ * The main reason for creating this is to improve performance; normally PHP will lock the session
+ * file for as long as your PHP request is running. However, we want to do multiple PHP requests at
+ * once. For this reason, we only use the PHP session system for login information, and use this
+ * class for other state information. This means that we can set a message as 'read' at the same time
+ * as opening a dialog to view a message.
+ *
+ * Currently, there is one 'state' for each 'subsystem'. The 'subsystem' is simply a tag which is appended
+ * to the request URL when the client does an XML request. Each 'subsystem' has its own state file.
+ *
+ * Currently the subsystem is equal to the module ID. This means that if you have two requests from the same
+ * module, they will have to wait for eachother. In practice this should hardly ever happen.
+ *
+ * @package core
+ */
+
+class State {
+
+	/**
+	 * The file pointer of the state file
+	 */
+	private $fp = false;
+
+	/**
+	 * The basedir in which the statefiles are found
+	 */
+	private $basedir;
+
+	/**
+	 * The filename which is opened by this state file
+	 */
+	private $filename;
+
+	/**
+	 * The directory in which the session files are created
+	 */
+	private $sessiondir = "session";
+
+	/**
+	 * The unserialized data as it has been read from the file
+	 */
+	public $sessioncache;
+
+	/**
+	 * The raw data as it has been read from the file
+	 */
+	public $contents;
+
+	/**
+	 * @param string $subsystem Name of the subsystem
+	 */
+	function __construct($subsystem) {
+		$this->basedir = TMP_PATH . DIRECTORY_SEPARATOR . $this->sessiondir;
+		$this->filename = $this->basedir . DIRECTORY_SEPARATOR . session_id() . "." . $subsystem;
+	}
+
+	/**
+	 * Open the session file
+	 *
+	 * The session file is opened and locked so that other processes can not access the state information
+	 */
+	function open() {
+		if($this->fp === false) {
+			if (!is_dir($this->basedir)) {
+				mkdir($this->basedir, 0755, true /* recursive */);
+			}
+			$this->fp = fopen($this->filename, "a+");
+			$this->sessioncache = false;
+			flock($this->fp, LOCK_EX);
+		}
+	}
+
+	/**
+	 * Read a setting from the state file
+	 *
+	 * @param string $name Name of the setting to retrieve
+	 * @return string Value of the state value, or null if not found
+	 */
+	function read($name) {
+		if($this->fp !== false) {
+			// If the file has already been read, we only have to access
+			// our cache to obtain the requeste data.
+			if ($this->sessioncache === false) {
+				$this->contents = file_get_contents($this->filename);
+				$this->sessioncache = unserialize($this->contents);
+			}
+
+			if (isset($this->sessioncache[$name])) {
+				return $this->sessioncache[$name];
+			}
+		} else {
+			dump('[STATE ERROR] State file "' . $this->filename . '" isn\'t opened, Please open state file before reading it."');
+		}
+		return false;
+	}
+
+	/**
+	 * Write a setting to the state file
+	 *
+	 * @param string $name Name of the setting to write
+	 * @param mixed $object Value of the object to be written to the setting
+	 * @param bool $flush False to prevent the changes written to disk
+	 * This requires a call to $flush() to write the changes to disk.
+	 */
+	function write($name, $object, $flush = true)
+	{
+		if($this->fp !== false) {
+			// If the file has already been read, then we don't
+			// need to read the entire file again.
+			if ($this->sessioncache === false) {
+				$this->read($name);
+			}
+
+			$this->sessioncache[$name] = $object;
+
+			if ($flush === true) {
+				$this->flush();
+			}
+		} else {
+			dump('[STATE ERROR] State file "' . $this->filename . '" isn\'t opened, Please open state file before writing on it."');
+		}
+	}
+
+	/**
+	 * Flushes all changes to disk
+	 *
+	 * This flushes all changed made to the $this->sessioncache to disk
+	 */
+	function flush()
+	{
+		if($this->fp !== false) {
+			if ($this->sessioncache) {
+				$contents = serialize($this->sessioncache);
+
+				if ($contents !== $this->contents) {
+					ftruncate($this->fp, 0);
+					fseek($this->fp, 0);
+					fwrite($this->fp, $contents);
+					$this->contents = $contents;
+				}
+			}
+		} else {
+			dump('[STATE ERROR] State file "' . $this->filename . '" isn\'t opened, Please open state file before writing on it."');
+		}
+	}
+
+	/**
+	 * Close the state file
+	 *
+	 * This closes and unlocks the state file so that other processes can access the state
+	 */
+	function close() {
+		if (isset($this->fp)) {
+			// release write lock -- fclose does this automatically
+			// but only in PHP <= 5.3.2
+			flock($this->fp, LOCK_UN);
+			fclose($this->fp);
+		}
+	}
+
+	/**
+	 * Cleans all old state information in the session directory
+	 * @param Integer $maxLifeTime The maximum allowed age of files in seconds.
+	 */
+	function clean($maxLifeTime = STATE_FILE_MAX_LIFETIME) {
+		cleanTemp($this->basedir, $maxLifeTime);
+	}
+}

commit 6f8c295b4dc905945006636cf3ee0cf3b5282782

storage: clarify resource lifetimes
Fixes: 2b7b06719
Fixes: grommunio-web-1.0-22-g7fe0804e5
Fixes: grommunio-web-3.17-275-g5fc1534a8
Fixes: grommunio-web-3.19-353-g40aed85b3
Fixes: grommunio-web-3.19-357-g96fac031e

server/includes/core/class.indexsqlite.php
@@ -377,8 +377,6 @@ class IndexSqlite extends SQLite3 {
 		$matchedRows = 0;
 		$sampleRows = [];
 		$entryids = [];
-		$stmt = null;
-		$results = null;
 
 		try {
 			$stmt = $this->prepare($sql);
@@ -455,10 +453,10 @@ class IndexSqlite extends SQLite3 {
 			}
 		}
 		finally {
-			if ($results instanceof SQLite3Result) {
+			if (isset($results) && $results !== false) {
 				$results->finalize();
 			}
-			if ($stmt instanceof SQLite3Stmt) {
+			if (isset($stmt) && $stmt !== false) {
 				$stmt->close();
 			}
 			// Always restore the original time limit, even after an error.

server/includes/core/class.state.php
@@ -21,16 +21,18 @@
  * Currently the subsystem is equal to the module ID. This means that if you have two requests from the same
  * module, they will have to wait for each other. In practice this should hardly ever happen.
  *
- * It can also support to create global state which can be access by all PHP request.
+ * It can also create global state that can be accessed by every PHP request.
  */
 class State {
 	/**
 	 * The file pointer of the state file.
+	 *
+	 * @var false|resource
 	 */
 	private $fp = false;
 
 	/**
-	 * The basedir in which the statefiles are found.
+	 * The base directory in which the state files are found.
 	 */
 	private $basedir;
 
@@ -56,11 +58,15 @@ class State {
 
 	/**
 	 * The unserialized data as it has been read from the file.
+	 *
+	 * @var array<string, mixed>
 	 */
 	public $sessioncache = [];
 
 	/**
 	 * The raw data as it has been read from the file.
+	 *
+	 * @var null|string
 	 */
 	public $contents;
 
@@ -88,7 +94,7 @@ class State {
 	/**
 	 * Open the session file.
 	 *
-	 * The session file is opened and locked so that other processes can not access the state information
+	 * The session file is opened and locked so that other processes cannot access the state information.
 	 *
 	 * @param int $retry Reopen attempts when clean() replaced the file while waiting for its lock
 	 *
@@ -154,8 +160,12 @@ class State {
 	 * @return bool true when the locked handle still is the file at $this->filename
 	 */
 	private function isLinked() {
+		if (!is_resource($this->fp)) {
+			return false;
+		}
+		$handle = $this->fp;
 		clearstatcache(true, $this->filename);
-		$open = fstat($this->fp);
+		$open = fstat($handle);
 		$disk = @stat($this->filename);
 
 		return $open !== false && $disk !== false && $open['ino'] === $disk['ino'] && $open['dev'] === $disk['dev'];
@@ -169,25 +179,30 @@ class State {
 	 * @return mixed Value of the state value, or null if not found
 	 */
 	public function read($name) {
-		if ($this->fp !== false) {
-			// If the file has already been read, we only have to access
-			// our cache to obtain the requeste data.
+		if (!is_resource($this->fp)) {
+			dump('[STATE ERROR] State file "' . $this->filename . '" is not open. Open it before reading.');
 			if (empty($this->sessioncache)) {
-				rewind($this->fp);
-				$contents = stream_get_contents($this->fp);
-				$this->contents = $contents === false ? '' : $contents;
-				$this->sessioncache = $this->contents === '' ? [] : unserialize($this->contents);
-				if (!is_array($this->sessioncache)) {
-					$this->sessioncache = [];
-				}
+				$this->sessioncache = [];
 			}
 
-			if (isset($this->sessioncache[$name])) {
-				return $this->sessioncache[$name];
+			return null;
+		}
+		$handle = $this->fp;
+
+		// If the file has already been read, we only have to access
+		// our cache to obtain the requested data.
+		if (empty($this->sessioncache)) {
+			rewind($handle);
+			$contents = stream_get_contents($handle);
+			$this->contents = $contents === false ? '' : $contents;
+			$this->sessioncache = $this->contents === '' ? [] : unserialize($this->contents);
+			if (!is_array($this->sessioncache)) {
+				$this->sessioncache = [];
 			}
 		}
-		else {
-			dump('[STATE ERROR] State file "' . $this->filename . '" isn\'t opened, Please open state file before reading it."');
+
+		if (isset($this->sessioncache[$name])) {
+			return $this->sessioncache[$name];
 		}
 		if (empty($this->sessioncache)) {
 			$this->sessioncache = [];
@@ -202,10 +217,10 @@ class State {
 	 * @param string $name   Name of the setting to write
 	 * @param mixed  $object Value of the object to be written to the setting
 	 * @param bool   $flush  false to prevent the changes written to disk
-	 *                       This requires a call to $flush() to write the changes to disk
+	 *                       This requires a call to flush() to write the changes to disk
 	 */
 	public function write($name, $object, $flush = true) {
-		if ($this->fp !== false) {
+		if (is_resource($this->fp)) {
 			// If the file has already been read, then we don't
 			// need to read the entire file again.
 			if (empty($this->sessioncache)) {
@@ -219,30 +234,31 @@ class State {
 			}
 		}
 		else {
-			dump('[STATE ERROR] State file "' . $this->filename . '" isn\'t opened, Please open state file before writing on it."');
+			dump('[STATE ERROR] State file "' . $this->filename . '" is not open. Open it before writing.');
 		}
 	}
 
 	/**
 	 * Flushes all changes to disk.
 	 *
-	 * This flushes all changed made to the $this->sessioncache to disk
+	 * This flushes all changes made to $this->sessioncache to disk.
 	 */
 	public function flush() {
-		if ($this->fp !== false) {
-			if (!empty($this->sessioncache)) {
-				$contents = serialize($this->sessioncache);
+		if (!is_resource($this->fp)) {
+			dump('[STATE ERROR] State file "' . $this->filename . '" is not open. Open it before writing.');
 
-				if ($contents !== $this->contents) {
-					ftruncate($this->fp, 0);
-					fseek($this->fp, 0);
-					fwrite($this->fp, $contents);
-					$this->contents = $contents;
-				}
-			}
+			return;
 		}
-		else {
-			dump('[STATE ERROR] State file "' . $this->filename . '" isn\'t opened, Please open state file before writing on it."');
+		$handle = $this->fp;
+		if (!empty($this->sessioncache)) {
+			$contents = serialize($this->sessioncache);
+
+			if ($contents !== $this->contents) {
+				ftruncate($handle, 0);
+				fseek($handle, 0);
+				fwrite($handle, $contents);
+				$this->contents = $contents;
+			}
 		}
 	}
 
@@ -252,13 +268,15 @@ class State {
 	 * This closes and unlocks the state file so that other processes can access the state
 	 */
 	public function close() {
-		if ($this->fp !== false) {
-			// release write lock -- fclose does this automatically
-			// but only in PHP <= 5.3.2
-			flock($this->fp, LOCK_UN);
-			fclose($this->fp);
-			$this->fp = false;
+		if (!is_resource($this->fp)) {
+			return;
 		}
+		$handle = $this->fp;
+		// release write lock -- fclose does this automatically
+		// but only in PHP <= 5.3.2
+		flock($handle, LOCK_UN);
+		fclose($handle);
+		$this->fp = false;
 	}
 
 	public function __destruct() {

server/test/indexSqliteResourceTest.php
@@ -1,0 +1,99 @@
+<?php
+
+if (!extension_loaded('sqlite3')) {
+	echo "SQLite resource checks skipped (sqlite3 is unavailable)\n";
+
+	return;
+}
+if (function_exists('mapi_linkmessages')) {
+	echo "SQLite resource checks skipped with php-mapi loaded\n";
+
+	return;
+}
+
+$temporaryDirectory = sys_get_temp_dir() . '/grommunio-index-' . bin2hex(random_bytes(8));
+$userDirectory = $temporaryDirectory . '/test-user';
+if (!mkdir($userDirectory, 0700, true)) {
+	throw new RuntimeException('Unable to create the SQLite test directory.');
+}
+
+define('SQLITE_INDEX_PATH', $temporaryDirectory);
+define('DEBUG_FULLTEXT_SEARCH', false);
+define('MAX_FTS_RESULT_ITEMS', 10);
+define('MAX_FTS_EXECUTION_TIME', 1);
+define('MAX_FTS_QUERY_TERMS', 10);
+define('SQLITE_FTS_TOKENIZER', 'unicode61');
+
+$linkCall = [];
+if (!function_exists('mapi_linkmessages')) {
+	function mapi_linkmessages($session, $searchEntryid, $entryids) {
+		$GLOBALS['linkCall'] = [$session, $searchEntryid, $entryids];
+	}
+}
+
+require_once dirname(__DIR__) . '/includes/core/class.indexsqlite.php';
+
+$databasePath = $userDirectory . '/index.sqlite3';
+
+try {
+	$database = new SQLite3($databasePath);
+	if (!$database->exec('CREATE VIRTUAL TABLE messages USING fts5(subject, body)') ||
+		!$database->exec('CREATE TABLE msg_content (' .
+			'message_id INTEGER PRIMARY KEY, entryid BLOB, folder_id INTEGER, ' .
+			'message_class TEXT, date INTEGER, readflag INTEGER, attach_indexed INTEGER)') ||
+		!$database->exec("INSERT INTO messages(rowid, subject, body) VALUES (1, 'Needle', 'Test body')")) {
+		throw new RuntimeException('Unable to create the SQLite search fixture.');
+	}
+
+	$statement = $database->prepare(
+		'INSERT INTO msg_content ' .
+		'(message_id, entryid, folder_id, message_class, date, readflag, attach_indexed) ' .
+		'VALUES (1, :entryid, 7, :message_class, 100, 0, 1)'
+	);
+	if ($statement === false ||
+		!$statement->bindValue(':entryid', "\x01\x02", SQLITE3_BLOB) ||
+		!$statement->bindValue(':message_class', 'IPM.Note', SQLITE3_TEXT)) {
+		throw new RuntimeException('Unable to prepare the SQLite search fixture.');
+	}
+	$result = $statement->execute();
+	if ($result === false) {
+		throw new RuntimeException('Unable to populate the SQLite search fixture.');
+	}
+	$resultFinalized = $result->finalize();
+	$statementClosed = $statement->close();
+	$databaseClosed = $database->close();
+	if (!$resultFinalized || !$statementClosed || !$databaseClosed) {
+		throw new RuntimeException('Unable to close the SQLite search fixture.');
+	}
+
+	$index = new IndexSqlite('test-user', 'test-session', 'test-store');
+	$descriptor = [
+		'ast' => [
+			'type' => 'term',
+			'fields' => ['subject'],
+			'value' => 'Needle',
+		],
+	];
+	if (!$index->is_open() || !$index->search('search-folder', $descriptor, null, false)) {
+		throw new RuntimeException('The SQLite search fixture could not be searched.');
+	}
+	if ($linkCall !== ['test-session', 'search-folder', ["\x01\x02"]]) {
+		throw new RuntimeException('The SQLite search did not release and link its matching result.');
+	}
+	if (!$index->close()) {
+		throw new RuntimeException('Unable to close the SQLite search index.');
+	}
+}
+finally {
+	if (is_file($databasePath) && !unlink($databasePath)) {
+		throw new RuntimeException('Unable to remove the SQLite test database.');
+	}
+	if (is_dir($userDirectory) && !rmdir($userDirectory)) {
+		throw new RuntimeException('Unable to remove the SQLite test user directory.');
+	}
+	if (is_dir($temporaryDirectory) && !rmdir($temporaryDirectory)) {
+		throw new RuntimeException('Unable to remove the SQLite test directory.');
+	}
+}
+
+echo "SQLite resource checks passed\n";

server/test/stateStorageTest.php
@@ -1,0 +1,71 @@
+<?php
+
+$temporaryDirectory = sys_get_temp_dir() . '/grommunio-state-' . bin2hex(random_bytes(8));
+if (!mkdir($temporaryDirectory, 0700)) {
+	throw new RuntimeException('Unable to create the state test directory.');
+}
+
+define('TMP_PATH', $temporaryDirectory);
+define('STATE_FILE_MAX_LIFETIME', 3600);
+
+$stateErrors = [];
+function dump($message) {
+	$GLOBALS['stateErrors'][] = $message;
+}
+
+require_once dirname(__DIR__) . '/includes/core/class.state.php';
+
+try {
+	$state = new State('roundtrip', 'test-owner');
+	if (!$state->open() || !$state->open()) {
+		throw new RuntimeException('The state file could not be opened idempotently.');
+	}
+
+	$payload = (object) ['name' => 'stored value'];
+	$state->write('payload', $payload, false);
+	$state->write('zero', 0, false);
+	$state->flush();
+	$state->close();
+	$state->close();
+
+	$restored = new State('roundtrip', 'test-owner');
+	if (!$restored->open()) {
+		throw new RuntimeException('The persisted state file could not be reopened.');
+	}
+	$restoredPayload = $restored->read('payload');
+	if (!$restoredPayload instanceof stdClass || $restoredPayload->name !== 'stored value') {
+		throw new RuntimeException('The object state did not survive a round trip.');
+	}
+	if ($restored->read('zero') !== 0 || $restored->read('missing') !== null) {
+		throw new RuntimeException('State values were not returned with their original types.');
+	}
+	$restored->close();
+
+	if ($restored->read('payload') !== null || count($stateErrors) !== 1) {
+		throw new RuntimeException('Reading a closed state did not fail safely.');
+	}
+}
+finally {
+	$sessionDirectory = $temporaryDirectory . '/session';
+	if (is_dir($sessionDirectory)) {
+		$files = scandir($sessionDirectory);
+		if ($files === false) {
+			throw new RuntimeException('Unable to inspect the state test directory.');
+		}
+		foreach ($files as $filename) {
+			if ($filename !== '.' && $filename !== '..') {
+				if (!unlink($sessionDirectory . '/' . $filename)) {
+					throw new RuntimeException('Unable to remove a state test file.');
+				}
+			}
+		}
+		if (!rmdir($sessionDirectory)) {
+			throw new RuntimeException('Unable to remove the state session directory.');
+		}
+	}
+	if (!rmdir($temporaryDirectory)) {
+		throw new RuntimeException('Unable to remove the state test directory.');
+	}
+}
+
+echo "State storage checks passed\n";