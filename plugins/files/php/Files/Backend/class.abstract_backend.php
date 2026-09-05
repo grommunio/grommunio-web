grommunio/grommunio-web

commit 2b7b06719e05ae419c5cd3542240b1f68faa1919

first upload of source codes

server/includes/modules/class.appointmentitemmodule.php
@@ -1,0 +1,328 @@
+<?php
+	require_once(BASE_PATH . 'server/includes/mapi/class.recurrence.php');
+	
+	/**
+	 * Appointment ItemModule
+	 * Module which openes, creates, saves and deletes an item. It 
+	 * extends the Module class.
+	 */
+	class AppointmentItemModule extends ItemModule
+	{
+		/**
+		 * Constructor
+		 * @param int $id unique id.
+		 * @param array $data list of all actions.
+		 */
+		function __construct($id, $data)
+		{
+			$this->properties = $GLOBALS['properties']->getAppointmentProperties();
+
+			parent::__construct($id, $data);
+
+			$this->plaintext = true;
+			$this->skipCopyProperties = array(
+				$this->properties['goid'],
+				$this->properties['goid2'],
+				$this->properties['request_sent'],
+				PR_OWNER_APPT_ID
+			);
+		}
+		
+		function open($store, $entryid, $action)
+		{
+			if($store && $entryid) {
+				$data = array();
+
+				$message = $GLOBALS['operations']->openMessage($store, $entryid);
+
+				if(empty($message)) {
+					return;
+				}
+
+				// Open embedded message if requested
+				$attachNum = !empty($action['attach_num']) ? $action['attach_num'] : false;
+				if($attachNum) {
+					// get message props of sub message
+					$parentMessage = $message;
+					$message = $GLOBALS['operations']->openMessage($store, $entryid, $attachNum);
+
+					if(empty($message)) {
+						return;
+					}
+
+					$data['item'] = $GLOBALS['operations']->getEmbeddedMessageProps($store, $message, $this->properties, $parentMessage, $attachNum);
+				} else {
+					// add all standard properties from the series/normal message 
+					$data['item'] = $GLOBALS['operations']->getMessageProps($store, $message, $this->properties, $this->plaintext);
+				}
+
+				// if appointment is recurring then only we should get properties of occurence if basedate is supplied
+				if($data['item']['props']['recurring'] === true) {
+					if(!empty($action['basedate'])) {
+						// check for occurence/exception
+						$basedate = $action['basedate'];
+
+						$recur = new Recurrence($store, $message);
+
+						$exceptionatt = $recur->getExceptionAttachment($basedate);
+
+						// Single occurences are never recurring
+						$data['item']['props']['recurring'] = false;
+
+						if($exceptionatt) {
+							// Existing exception (open existing item, which includes basedate)
+							$exceptionattProps = mapi_getprops($exceptionatt, array(PR_ATTACH_NUM));
+							$exception = mapi_attach_openobj($exceptionatt, 0);
+
+							// overwrite properties with the ones from the exception
+							$exceptionProps = $GLOBALS['operations']->getMessageProps($store, $exception, $this->properties, $this->plaintext);
+
+							/**
+							 * If recurring item has set reminder to true then
+							 * all occurrences before the 'flagdueby' value(of recurring item)
+							 * should not show that reminder is set.
+							 */
+							if (isset($exceptionProps['props']['reminder']) && $data['item']['props']['reminder'] == true) {
+								$flagDueByDay = $recur->dayStartOf($data['item']['props']['flagdueby']);
+
+								if ($flagDueByDay > $basedate) {
+									$exceptionProps['props']['reminder'] = false;
+								}
+							}
+
+							// The properties must be merged, if the recipients or attachments are present in the exception
+							// then that list should be used. Otherwise the list from the series must be applied (this
+							// corresponds with OL2007).
+							// @FIXME getMessageProps should not return empty string if exception doesn't contain body
+							// by this change we can handle a situation where user has set empty string in the body explicitly
+							if (!empty($exceptionProps['props']['body']) || !empty($exceptionProps['props']['html_body'])) {
+								if(!empty($exceptionProps['props']['body'])) {
+									$data['item']['props']['body'] = $exceptionProps['props']['body'];
+								}
+
+								if(!empty($exceptionProps['props']['html_body'])) {
+									$data['item']['props']['html_body'] = $exceptionProps['props']['html_body'];
+								}
+
+								$data['item']['props']['isHTML'] = $exceptionProps['props']['isHTML'];
+							}
+							// remove properties from $exceptionProps so array_merge will not overwrite it
+							unset($exceptionProps['props']['html_body']);
+							unset($exceptionProps['props']['body']);
+							unset($exceptionProps['props']['isHTML']);
+
+							$data['item']['props'] = array_merge($data['item']['props'], $exceptionProps['props']);
+							if (isset($exceptionProps['recipients'])) {
+								$data['item']['recipients'] = $exceptionProps['recipients'];
+							}
+
+							if (isset($exceptionProps['attachments'])) {
+								$data['item']['attachments'] = $exceptionProps['attachments'];
+							}
+
+							// Make sure we are using the passed basedate and not something wrong in the opened item
+							$data['item']['props']['basedate'] = $basedate;
+							$data['item']['attach_num'] = array($exceptionattProps[PR_ATTACH_NUM]);
+						} else if($recur->isDeleteException($basedate)) {
+							// Exception is deleted, should not happen, but if it the case then give error
+							$this->sendFeedback(false,
+								array(
+									'type' => ERROR_ZARAFA,
+									'info' => array(
+										'original_message' => Language::getstring('Could not open occurrence.'),
+										'display_message' => Language::getstring('Could not open occurrence, specific occurrence is probably deleted.')
+									)
+								)
+							);
+							return;
+						} else {
+							// opening an occurence of a recurring series (same as normal open, but add basedate, startdate and enddate)
+							$data['item']['props']['basedate'] = $basedate;
+							$data['item']['props']['startdate'] = $recur->getOccurrenceStart($basedate);
+							$data['item']['props']['duedate'] = $recur->getOccurrenceEnd($basedate);
+							$data['item']['props']['commonstart'] = $data['item']['props']['startdate'];
+							$data['item']['props']['commonend'] = $data['item']['props']['duedate'];
+							unset($data['item']['props']['reminder_time']);
+
+							/**
+							 * If recurring item has set reminder to true then
+							 * all occurrences before the 'flagdueby' value(of recurring item)
+							 * should not show that reminder is set.
+							 */
+							if (isset($exceptionProps['props']['reminder']) && $data['item']['props']['reminder'] == true) {
+								$flagDueByDay = $recur->dayStartOf($data['item']['props']['flagdueby']);
+
+								if ($flagDueByDay > $basedate) {
+									$exceptionProps['props']['reminder'] = false;
+								}
+							}
+						}
+					} else {
+						// Opening a recurring series, get the recurrence information
+						$recur = new Recurrence($store, $message);
+						$recurpattern = $recur->getRecurrence();
+						$tz = $recur->tz; // no function to do this at the moment
+
+						// Add the recurrence pattern to the data
+						if(isset($recurpattern) && is_array($recurpattern)) {
+							$data['item']['props'] += $recurpattern;
+						}
+
+						// Add the timezone information to the data
+						if(isset($tz) && is_array($tz)) {
+							$data['item']['props'] += $tz;
+						}
+					}
+				}
+
+				// Send the data
+				$this->addActionData('item', $data);
+				$GLOBALS['bus']->addData($this->getResponseData());
+			}
+		}
+
+		/**
+		 * Function does customization of exception based on module data.
+		 * like, here it will generate display message based on actionType
+		 * for particular exception.
+		 *
+		 * @param object $e Exception object
+		 * @param string $actionType the action type, sent by the client
+		 * @param MAPIobject $store Store object of message.
+		 * @param string $parententryid parent entryid of the message.
+		 * @param string $entryid entryid of the message.
+		 * @param array $action the action data, sent by the client
+		 */
+		function handleException(&$e, $actionType = null, $store = null, $parententryid = null, $entryid = null, $action = null)
+		{
+			if (is_null($e->displayMessage)) {
+				switch ($actionType) {
+					case "save":
+						if ($e->getCode() == MAPI_E_NO_ACCESS) {
+							$message = mapi_msgstore_openentry($store, $entryid);
+							$messageProps = mapi_getprops($message, array(PR_MESSAGE_CLASS, PR_ENTRYID, PR_PARENT_ENTRYID, PR_STORE_ENTRYID));
+							$messageClass = $messageProps[PR_MESSAGE_CLASS];
+
+							$text = $messageClass !== "IPM.Appointment" ? Language::getstring('a meeting request') : Language::getstring('an appointment');
+							$msg = Language::getstring('You have insufficient privileges to move ' . $text .' in this calendar. The calendar owner can set these using the \'permissions\'-tab of the folder properties (right click the calendar folder > properties > permissions)');
+
+							$e->setDisplayMessage($msg);
+							$e->setTitle(Language::getstring('Insufficient privileges'));
+
+							// Need this notification to refresh the calender.
+							$GLOBALS['bus']->notify(bin2hex($parententryid), TABLE_DELETE, $messageProps);
+						}
+						break;
+				}
+			}
+			parent::handleException($e, $actionType, $store, $parententryid, $entryid, $action);
+		}
+
+
+		/**
+		 * Save the give appointment or meeting request to the calendar.
+		 *
+		 * @param mapistore $store MAPI store of the message
+		 * @param string $parententryid Parent entryid of the message (folder entryid, NOT message entryid)
+		 * @param string $entryid entryid of the message
+		 * @param array $action Action array containing json request
+		 * @param string $actionType The action type which triggered this action
+		 */
+		function save($store, $parententryid, $entryid, $action, $actionType = 'save')
+		{
+			$result = false;
+
+			// Save appointment (saveAppointment takes care of creating/modifying exceptions to recurring
+			// items if necessary)
+			$messageProps = $GLOBALS['operations']->saveAppointment($store, $entryid, $parententryid, $action, $actionType, $this->directBookingMeetingRequest);
+
+			// Notify the bus if the save was OK
+			if($messageProps && !(is_array($messageProps) && isset($messageProps['error'])) && !isset($messageProps['remindertimeerror']) ){
+				$GLOBALS['bus']->notify(bin2hex($parententryid), TABLE_SAVE, $messageProps);
+				$result = true;
+			}
+
+			$errorMsg = false;
+			if(!$result && isset($messageProps['remindertimeerror']) && !$messageProps['remindertimeerror']){
+				$errorMsg = Language::getstring('Cannot set a reminder to appear before the previous occurrence. Reset reminder to save the change');
+			}else if (isset($messageProps['isexceptionallowed']) && $messageProps['isexceptionallowed'] === false){
+				$errorMsg = Language::getstring('Two occurrences cannot occur on the same day');
+			}elseif(is_array($messageProps) && isset($messageProps['error'])){
+				switch($messageProps['error']){
+					case 1:
+						$errorMsg = sprintf(Language::getstring('You marked \'%s\' as a resource. You cannot schedule a meeting with \'%s\' because you do not have the appropiate permissions for that account. Either enter the name as a required or optional attendee or talk to your administrator about giving you permission to schedule \'%s\'.'), $messageProps['displayname'], $messageProps['displayname'], $messageProps['displayname']);
+						break;
+					case 2:
+						$errorMsg = sprintf(Language::getstring('\'%s\' has declined your meeting because \'%s\' does not automatically accept meeting requests.'), $messageProps['displayname'], $messageProps['displayname']);
+						break;
+					case 3:
+						$errorMsg = sprintf(Language::getstring('\'%s\' has declined your meeting because it is recurring. You must book each meeting separately with this resource.'), $messageProps['displayname']);
+						break;
+					case 4:
+						$errorMsg = sprintf(Language::getstring('\'%s\' is already booked for this specified time. You must use another time or find another resource.'), $messageProps['displayname']);
+						break;
+					default:
+						$errorMsg = Language::getstring('Meeting was not scheduled.');
+						break;
+				}
+			}else{
+				// Recurring but non-existing exception (same as normal open, but add basedate, startdate and enddate)
+				$data = array();
+				if ($result) {
+					$data = Conversion::mapMAPI2XML($this->properties, $messageProps);
+
+					// Get recipient information from the saved appointment to update client side
+					// according to the latest recipient related changes only if changes requested from client.
+					$savedAppointment = $GLOBALS['operations']->openMessage($store, $messageProps[PR_ENTRYID]);
+					if(!empty($action['recipients'])) {
+						$recipients = $GLOBALS["operations"]->getRecipientsInfo($savedAppointment);
+						if(!empty($recipients)) {
+							$data["recipients"] = array(
+								"item" => $recipients
+							);
+						}
+					}
+
+					// Get attachments information from the saved appointment to update client side
+					// according to the latest attachments related changes only if changes requested from client.
+					if (!empty($action['attachments'])) {
+						$attachments = $GLOBALS["operations"]->getAttachmentsInfo($savedAppointment);
+						if (!empty($attachments)) {
+							$data["attachments"] = array(
+								"item" => $attachments
+							);
+						}
+					}
+
+					$data['action_response'] = Array(
+						'resources_booked' => $this->directBookingMeetingRequest
+					);
+
+					if(isset($action['message_action']) && isset($action['message_action']['paste'])) {
+						$data['action_response']['resources_pasted'] = true;
+					}
+
+				} else {
+					if(!empty($action['message_action']['send'])){
+						$errorMsg = Language::getstring('Meeting could not be sent.');
+					}else{
+						$errorMsg = Language::getstring('Meeting could not be saved.');
+					}
+				}
+			}
+
+			if($errorMsg===false){
+
+				$this->addActionData('update', array('item' => $data));
+				$GLOBALS['bus']->addData($this->getResponseData());
+			} else {
+				$this->sendFeedback(false, array(
+					'type' => ERROR_ZARAFA,
+					'info' => array(
+						'display_message' => $errorMsg,
+					)
+				));
+			}
+		}
+	}
+?>

server/includes/notifiers/class.notifier.php
@@ -1,0 +1,103 @@
+<?php
+	/**
+	 * Notifier
+	 *
+	 * Superclass for every notifier.
+	 */
+	class Notifier {
+		/**
+		 * @var array list of the notifications, which is send to the client.
+		 */
+		private $responseNotificationData;
+
+		/**
+		 * @var int notification counter of the class
+		 */
+		private $notifyCount;
+
+		public function __construct()
+		{
+			$this->notifyCount = 0;
+			$this->responseNotificationData = array();
+		}
+
+		/**
+		 * @return Number Return the bitmask of events which are handled
+		 * by this notifier. The bitmask can consist of the 
+		 * OBJECT_SAVE, OBJECT_DELETE, TABLE_SAVE, TABLE_DELETE, REQUEST_START and REQUEST_END flags
+		 */
+		public function getEvents()
+		{
+			return 0;
+		}
+
+		/**
+		 * Function which resets the data and the response data class variable.
+		 */
+		public function reset()
+		{
+			$this->notifyCount = 0;
+			$this->responseNotificationData = array();
+		}
+
+		/**
+		 * Function which returns name of the notifier class.
+		 * @return string notifier name.
+		 */
+		public function getNotifierName()
+		{
+			return strtolower(get_class($this));
+		}
+
+		/**
+		 * Function which adds notification data to module, so later it can be retrieved to send.
+		 * @param string $actionType type of action that response data corresponds.
+		 * @return array data object.
+		 */
+		protected function addNotificationActionData($actionType, $data)
+		{
+			if (!isset($this->responseNotificationData[$actionType])) {
+				$this->responseNotificationData[$actionType] = $data;
+			}
+		}
+
+		/**
+		 * Function which returns notification data that will be sent to client. If there isn't any data added
+		 * to response data then it will return a blank array.
+		 * @return object response data.
+		 */
+		protected function createNotificationResponseData()
+		{
+			if (!empty($this->responseNotificationData)) {
+				$moduleName = $this->getNotifierName();
+				$idName = $moduleName . (++$this->notifyCount);
+
+				$response = array(
+					$moduleName => array(
+						$idName => $this->responseNotificationData
+					)
+				);
+
+				// Clear notification data, the response which is being returned
+				// should be sent to the client.
+				$this->responseNotificationData = array();
+
+				return $response;
+			} else {
+				return array();
+			}
+		}
+
+		/**
+		 * If an event elsewhere has occurred, it enters in this methode. This method
+		 * executes one ore more actions, depends on the event.
+		 * @param int $event Event.
+		 * @param string $entryid Entryid.
+		 * @param array $data array of data.
+		 */
+		public function update($event, $entryid, $data)
+		{
+			// you must implement this function for each notifier
+		}
+	}
+?>

commit 3ef532f10b925ffd64c349b27f0a912da65a862f

all: document callback parameter contracts
Fixes: grommunio-web-2.8-61-gbf45991cb
Fixes: grommunio-web-3.10-48-g015d29bf5
Fixes: 2b7b06719
Fixes: grommunio-web-3.2-33-g63ef83829
Fixes: grommunio-web-3.19-355-g6c5c36f75

plugins/files/php/Files/Backend/class.abstract_backend.php
@@ -8,6 +8,8 @@
 
 namespace Files\Backend;
 
+use Files\Core\Account;
+
 require_once __DIR__ . "/interface.quota.php";
 require_once __DIR__ . "/interface.version.php";
 require_once __DIR__ . "/interface.streaming.php";
@@ -356,11 +358,13 @@ abstract class AbstractBackend {
 	}
 
 	/**
-	 * This function gets called before the backend-account is deleted.
+	 * Hook invoked before a backend account is deleted.
 	 *
-	 * @param mixed $account
+	 * Backends may override this method to clean up account-specific data.
+	 *
+	 * @param Account $account account being deleted
 	 */
-	public function beforeDeleteAccount($account) {
+	public function beforeDeleteAccount(/* @scrutinizer ignore-unused */ $account) {
 		// do nothing by default
 	}
 

server/includes/core/class.mapisession.php
@@ -683,15 +683,13 @@ class MAPISession {
 	/**
 	 * Get single store if we are opening full store.
 	 *
-	 * @param object $store        the store of the user
-	 * @param array  $storeOptions contains folder_type of which folder to open
-	 *                             It is mapped to username, If folder_type is 'all' (i.e. Open Entire Inbox)
-	 *                             then we will open full store.
-	 * @param string $username     The username
+	 * @param resource    $store        the store of the user
+	 * @param null|array  $storeOptions hierarchy options retained for caller compatibility
+	 * @param null|string $username     store owner retained for caller compatibility
 	 *
 	 * @return array storeArray The array of stores containing user's store
 	 */
-	public function getSingleMessageStores($store, $storeOptions, $username) {
+	public function getSingleMessageStores($store, /* @scrutinizer ignore-unused */ $storeOptions, /* @scrutinizer ignore-unused */ $username) {
 		return [$store];
 	}
 

server/includes/loader.php
@@ -59,11 +59,14 @@ class FileLoader {
 	/**
 	 * Obtain the list of Extjs & UX CSS files.
 	 *
-	 * @param number $load the LOAD_RELEASE | LOAD_DEBUG | LOAD_SOURCE flag
+	 * The stylesheet is shared by all load modes, but this method follows the
+	 * same mode-aware public contract as the other loader methods.
+	 *
+	 * @param int $load the LOAD_RELEASE | LOAD_DEBUG | LOAD_SOURCE flag
 	 *
 	 * @return array The array of CSS files
 	 */
-	public function getExtjsCSSFiles($load) {
+	public function getExtjsCSSFiles(/* @scrutinizer ignore-unused */ $load) {
 		return ["client/extjs/resources/css/ext-all-ux.css"];
 	}
 

server/includes/modules/class.appointmentitemmodule.php
@@ -366,11 +366,11 @@ class AppointmentItemModule extends ItemModule {
 	/**
 	 * Processes an all-day item and calculates the correct starttime if necessary.
 	 *
-	 * @param object $store
-	 * @param array  $calendaritem
-	 * @param object $message
+	 * @param resource $store
+	 * @param array    $calendaritem
+	 * @param resource $message
 	 */
-	private function processAllDayItem($store, &$calendaritem, $message) {
+	private function processAllDayItem(/* @scrutinizer ignore-unused */ $store, &$calendaritem, /* @scrutinizer ignore-unused */ $message) {
 		// If the appointment doesn't have tzdefstart property, it was probably
 		// created on a mobile device (mobile devices do not send a timezone for
 		// all-day events).

server/includes/modules/class.delegatesmodule.php
@@ -279,11 +279,11 @@ class DelegatesModule extends Module {
 	 * Function will return information of a particular delegate from current user's store.
 	 *
 	 * @param string      $userEntryId         entryid of the delegate
-	 * @param array|false $delegateMeetingRule optional meeting-rule information, or false when no rule exists
+	 * @param array|false $delegateMeetingRule meeting rule retained for caller compatibility
 	 *
 	 * @return array delegate information
 	 */
-	public function getDelegatePermissions($userEntryId, $delegateMeetingRule = false) {
+	public function getDelegatePermissions($userEntryId, /* @scrutinizer ignore-unused */ $delegateMeetingRule = false) {
 		$delegateProps = $this->getDelegateProps();
 		$delegateIndex = $this->getDelegateIndex($userEntryId);
 		$userinfo = $this->getUserInfo($userEntryId);

server/includes/notifiers/class.notifier.php
@@ -41,9 +41,11 @@ class Notifier {
 	/**
 	 * Whether updates need serialized persistent notifier state.
 	 *
+	 * Specialized notifiers may make this decision based on the event.
+	 *
 	 * @param null|mixed $event
 	 */
-	public function usePersistentStateLock($event = null) {
+	public function usePersistentStateLock(/* @scrutinizer ignore-unused */ $event = null) {
 		static $statefulClasses = [];
 		$className = get_class($this);
 		if (isset($statefulClasses[$className])) {
@@ -115,11 +117,13 @@ class Notifier {
 	 * If an event elsewhere has occurred, it enters in this method. This method
 	 * executes one or more actions, depends on the event.
 	 *
+	 * Implementations receive all three values; the default hook is intentionally empty.
+	 *
 	 * @param int    $event   event
 	 * @param string $entryid entryid
 	 * @param array  $data    array of data
 	 */
-	public function update($event, $entryid, $data) {
+	public function update(/* @scrutinizer ignore-unused */ $event, /* @scrutinizer ignore-unused */ $entryid, /* @scrutinizer ignore-unused */ $data) {
 		// you must implement this function for each notifier
 	}
 }