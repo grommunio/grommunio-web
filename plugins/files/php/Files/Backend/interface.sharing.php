grommunio/grommunio-web

commit 2b7b06719e05ae419c5cd3542240b1f68faa1919

first upload of source codes

server/includes/core/class.colors.php
@@ -1,0 +1,431 @@
+<?php
+
+/**
+ * Utility class for color conversions
+ */
+class Colors {
+	/**
+	 * Creates a lighter color
+	 * @param String|Object $color A hexstring or color object (array with r, g, b keys)
+	 * @param Float $percentage the percentage with which the luminance of the color
+	 * should be changed
+	 * @return String A hexstring with the new color (including '#')
+	 */
+	public static function lighter($color, $percentage) {
+		$hsl = Colors::rgb2hsl($color);
+		$hsl['l'] += $percentage;
+		if ( $hsl['l'] > 100 ) {
+			$hsl['l'] = 100;
+		} elseif ( $hsl['l'] < 0 ) {
+			$hsl['l'] = 0;
+		}
+
+		$rgb = Colors::hsl2rgb($hsl);
+		return Colors::colorObject2string($rgb);
+	}
+
+	/**
+	 * Creates a darker color
+	 * @param String|Object $color A hexstring or color object (array with r, g, b keys)
+	 * @param Float $percentage the percentage with which the luminance of the color
+	 * should be changed
+	 * @return String A hexstring with the new color (including '#')
+	 */
+	public static function darker($color, $percentage) {
+		return Colors::lighter($color, -$percentage);
+	}
+
+	/**
+	 * Sets the luminance to a certain value.
+	 * @param String|Object $color A hexstring or color object (array with r, g, b keys)
+	 * @param Float $percentage the percentage to which the luminance of the color
+	 * should be set
+	 * @return String A hexstring with the new color (including '#')
+	 */
+	public static function setLuminance($color, $percentage) {
+		$hsl = Colors::rgb2hsl($color);
+		$hsl['l'] = $percentage;
+		if ( $hsl['l'] > 100 ) {
+			$hsl['l'] = 100;
+		} elseif ( $hsl['l'] < 0 ) {
+			$hsl['l'] = 0;
+		}
+
+		$rgb = Colors::hsl2rgb($hsl);
+		return Colors::colorObject2string($rgb);
+	}
+
+	/**
+	 * Transforms a hexstring into a color object (array with r, g, b keys)
+	 * @param String $color A hexstring or color object (array with r, g, b keys)
+	 * @return Array An array with values for r, g, b
+	 */
+	public static function colorString2Object($color) {
+		if ( substr($color, 0, 1) === '#' ) {
+			$color = substr($color, 1);
+		}
+
+		if ( strlen($color) === 3 ) {
+			$red = intval(substr($color, 0, 1).substr($color, 0, 1), 16);
+			$green = intval(substr($color, 1, 1).substr($color, 1, 1), 16);
+			$blue = intval(substr($color, 2, 1).substr($color, 2, 1), 16);
+		} else {
+			$red = intval(substr($color, 0, 2), 16);
+			$green = intval(substr($color, 2, 2), 16);
+			$blue = intval(substr($color, 4, 2), 16);
+		}
+
+		return [
+			'r' => $red,
+			'g' => $green,
+			'b' => $blue,
+		];
+	}
+
+	/**
+	 * Transforms a color object (array with r, g, b keys) into a hexstring
+	 * @param Array $color An array with values for r, g, b
+	 * @return String  A hexstring or color object (array with r, g, b keys)
+	 */
+	public static function colorObject2string($color) {
+		$r = dechex($color['r']);
+		if ( strlen($r) === 1 ) {
+			$r = '0'.$r;
+		}
+		$g = dechex($color['g']);
+		if ( strlen($g) === 1 ) {
+			$g = '0'.$g;
+		}
+		$b = dechex($color['b']);
+		if ( strlen($b) === 1 ) {
+			$b = '0'.$b;
+		}
+		return '#' . $r . $g . $b;
+	}
+
+	/**
+	 * Transforms an red-green-blue color to a hue-saturation-luminence color object
+	 * See http://www.niwa.nu/2013/05/math-behind-colorspace-conversions-rgb-hsl/ for
+	 * more information
+	 * @param Array $color An array with values for r, g, b
+	 * @return Array An array with values for h, s, l
+	 */
+	public static function rgb2hsl($color) {
+		if ( is_string($color) ) {
+			$color = Colors::colorString2Object($color);
+		}
+
+		$min = min([$color['r'], $color['g'], $color['b']]);
+		$max = max([$color['r'], $color['g'], $color['b']]);
+		$lum = (100/255) * ($min + $max) / 2;
+
+		if ( $min === $max ) {
+			// greyscale
+			return [
+				'h' => 0,
+				's' => 0,
+				'l' => $lum,
+			];
+		}
+
+		if ( $lum < 50 ) {
+			$sat = 100*($max-$min)/($max+$min);
+		} else {
+			$sat = 100*($max/255-$min/255)/(2-$max/255-$min/255);
+		}
+
+		if ( $max === $color['r'] ) {
+			$hue = ($color['g']-$color['b'])/($max-$min);
+		} elseif ( $max === $color['g'] ) {
+			$hue = 2 + ($color['b']-$color['r'])/($max-$min);
+		} else {
+			$hue = 4 + ($color['r']-$color['g'])/($max-$min);
+		}
+		$hue *= 60;
+		if ( $hue < 0 ) {
+			$hue += 360;
+		}
+
+		return [
+			'h' => $hue,
+			's' => $sat,
+			'l' => $lum,
+		];
+	}
+
+	/**
+	 * Transforms an hue-saturation-luminence to a red-green-blue color color object
+	 * See http://www.niwa.nu/2013/05/math-behind-colorspace-conversions-rgb-hsl/ for
+	 * more information
+	 * @param Array $color An array with values for h, s, l
+	 * @return Array An array with values for r, g, b
+	 */
+	public static function hsl2rgb($color) {
+		$hue = $color['h'];
+		$sat = $color['s'];
+		$lum = $color['l'];
+
+		if ( $color['s'] === 0 ) {
+			// greyscale
+			return [
+				'r' => $lum * (255/100),
+				'g' => $lum * (255/100),
+				'b' => $lum * (255/100),
+			];
+		}
+
+		$temp1 = $lum/100 < 0.5 ? ($lum/100)*(1+$sat/100) : ($lum+$sat-$lum*$sat/100)/100;
+		$temp2 = 2*$lum/100 - $temp1;
+
+		$tempr = $hue/360 + 0.333;
+		if ( $tempr > 1 ) {
+			$tempr -= 1;
+		}
+		$tempg = $hue/360;
+		$tempb = $hue/360 - 0.333;
+		if ( $tempb < 0 ) {
+			$tempb += 1;
+		}
+
+		if ( 6*$tempr < 1 ) {
+			$red = $temp2 + ($temp1-$temp2)*6*$tempr;
+		} elseif ( 2*$tempr < 1 ) {
+			$red = $temp1;
+		} elseif ( 3*$tempr < 2 ) {
+			$red = $temp2 + ($temp1-$temp2)*(0.666-$tempr)*6;
+		} else {
+			$red = $temp2;
+		}
+
+		if ( 6*$tempg < 1 ) {
+			$green = $temp2 + ($temp1-$temp2)*6*$tempg;
+		} elseif ( 2*$tempg < 1 ) {
+			$green = $temp1;
+		} elseif ( 3*$tempg < 2 ) {
+			$green = $temp2 + ($temp1-$temp2)*(0.666-$tempg)*6;
+		} else {
+			$green = $temp2;
+		}
+
+		if ( 6*$tempb < 1 ) {
+			$blue = $temp2 + ($temp1-$temp2)*6*$tempb;
+		} elseif ( 2*$tempb < 1 ) {
+			$blue = $temp1;
+		} elseif ( 3*$tempb < 2 ) {
+			$blue = $temp2 + ($temp1-$temp2)*(0.666-$tempb)*6;
+		} else {
+			$blue = $temp2;
+		}
+
+		return [
+			'r' => 255*$red,
+			'g' => 255*$green,
+			'b' => 255*$blue,
+		];
+	}
+
+	/**
+	* Returns the luma value (brightness perception)
+	* See http://stackoverflow.com/a/12043228
+	* @param String|Array $color A hexstring or a color object (rgb or hsl)
+	* @return Float luma value
+	*/
+	static public function getLuma($color)
+	{
+		if ( is_string($color) ) {
+			$color = Colors::colorString2Object($color);
+		} elseif ( !isset($color['r']) && isset($color['h']) ) {
+			// assume that this is an hsl color
+			$color = Colors::hsl2rgb($color);
+		}
+
+		$luma = 0.2126 * $color['r'] + 0.7152 * $color['g'] + 0.0722 * $color['b']; // per ITU-R BT.709
+
+		return $luma;
+	}
+
+	/**
+	 * Translates a css color value to a hex color value. So rgb, rgba and
+	 * color keywords will be translated to their hex counterparts.
+	 * @param String $color The color that needs to be translated
+	 * @return String The hex color value
+	 */
+	static public function getHexColorFromCssColor($color) {
+		$color = strtolower($color);
+		if ( isset(Colors::$keyWords[$color]) ) {
+			return Colors::$keyWords[$color];
+		}
+		if ( preg_match('/^#([0-9a-f]{3}|[0-9a-f]{4}|[0-9a-f]{6}|[0-9a-f]{8})$/', $color) ) {
+			// Already a hex color
+			return $color;
+		}
+		if ( preg_match('/^rgb\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)$/', $color, $m) ) {
+			$r = intval($m[1]);
+			$g = intval($m[2]);
+			$b = intval($m[3]);
+			if ( $r < 0 || $r > 255 || $g < 0 || $g > 255 | $b < 0 || $b > 255 ) {
+				// Not a valid color
+				return null;
+			}
+			return Colors::colorObject2string(['r'=>$r, 'g'=>$g, 'b'=>$b]);
+		}
+
+		// Not a valid color
+		// Note: alpha-numeric channels are not supported (i.e. rgba(x, x, x, x) or #xxxxxxxx or #xxxx)
+		return null;
+	}
+
+	/**
+	 * Color keywords. Taken from https://developer.mozilla.org/en-US/docs/Web/CSS/color_value
+	 */
+	static public $keyWords = [
+		'black' => '#000000',
+		'silver' => '#c0c0c0',
+		'gray' => '#808080',
+		'white' => '#ffffff',
+		'maroon' => '#800000',
+		'red' => '#ff0000',
+		'purple' => '#800080',
+		'fuchsia' => '#ff00ff',
+		'green' => '#008000',
+		'lime' => '#00ff00',
+		'olive' => '#808000',
+		'yellow' => '#ffff00',
+		'navy' => '#000080',
+		'blue' => '#0000ff',
+		'teal' => '#008080',
+		'aqua' => '#00ffff',
+		'orange' => '#ffa500',
+		'aliceblue' => '#f0f8ff',
+		'antiquewhite' => '#faebd7',
+		'aquamarine' => '#7fffd4',
+		'azure' => '#f0ffff',
+		'beige' => '#f5f5dc',
+		'bisque' => '#ffe4c4',
+		'blanchedalmond' => '#ffebcd',
+		'blueviolet' => '#8a2be2',
+		'brown' => '#a52a2a',
+		'burlywood' => '#deb887',
+		'cadetblue' => '#5f9ea0',
+		'chartreuse' => '#7fff00',
+		'chocolate' => '#d2691e',
+		'coral' => '#ff7f50',
+		'cornflowerblue' => '#6495ed',
+		'cornsilk' => '#fff8dc',
+		'crimson' => '#dc143c',
+		'cyan' => '#00ffff',
+		'darkblue' => '#00008b',
+		'darkcyan' => '#008b8b',
+		'darkgoldenrod' => '#b8860b',
+		'darkgray' => '#a9a9a9',
+		'darkgreen' => '#006400',
+		'darkgrey' => '#a9a9a9',
+		'darkkhaki' => '#bdb76b',
+		'darkmagenta' => '#8b008b',
+		'darkolivegreen' => '#556b2f',
+		'darkorange' => '#ff8c00',
+		'darkorchid' => '#9932cc',
+		'darkred' => '#8b0000',
+		'darksalmon' => '#e9967a',
+		'darkseagreen' => '#8fbc8f',
+		'darkslateblue' => '#483d8b',
+		'darkslategray' => '#2f4f4f',
+		'darkslategrey' => '#2f4f4f',
+		'darkturquoise' => '#00ced1',
+		'darkviolet' => '#9400d3',
+		'deeppink' => '#ff1493',
+		'deepskyblue' => '#00bfff',
+		'dimgray' => '#696969',
+		'dimgrey' => '#696969',
+		'dodgerblue' => '#1e90ff',
+		'firebrick' => '#b22222',
+		'floralwhite' => '#fffaf0',
+		'forestgreen' => '#228b22',
+		'gainsboro' => '#dcdcdc',
+		'ghostwhite' => '#f8f8ff',
+		'gold' => '#ffd700',
+		'goldenrod' => '#daa520',
+		'greenyellow' => '#adff2f',
+		'grey' => '#808080',
+		'honeydew' => '#f0fff0',
+		'hotpink' => '#ff69b4',
+		'indianred' => '#cd5c5c',
+		'indigo' => '#4b0082',
+		'ivory' => '#fffff0',
+		'khaki' => '#f0e68c',
+		'lavender' => '#e6e6fa',
+		'lavenderblush' => '#fff0f5',
+		'lawngreen' => '#7cfc00',
+		'lemonchiffon' => '#fffacd',
+		'lightblue' => '#add8e6',
+		'lightcoral' => '#f08080',
+		'lightcyan' => '#e0ffff',
+		'lightgoldenrodyellow' => '#fafad2',
+		'lightgray' => '#d3d3d3',
+		'lightgreen' => '#90ee90',
+		'lightgrey' => '#d3d3d3',
+		'lightpink' => '#ffb6c1',
+		'lightsalmon' => '#ffa07a',
+		'lightseagreen' => '#20b2aa',
+		'lightskyblue' => '#87cefa',
+		'lightslategray' => '#778899',
+		'lightslategrey' => '#778899',
+		'lightsteelblue' => '#b0c4de',
+		'lightyellow' => '#ffffe0',
+		'limegreen' => '#32cd32',
+		'linen' => '#faf0e6',
+		'magenta' => '#ff00ff',
+		'mediumaquamarine' => '#66cdaa',
+		'mediumblue' => '#0000cd',
+		'mediumorchid' => '#ba55d3',
+		'mediumpurple' => '#9370db',
+		'mediumseagreen' => '#3cb371',
+		'mediumslateblue' => '#7b68ee',
+		'mediumspringgreen' => '#00fa9a',
+		'mediumturquoise' => '#48d1cc',
+		'mediumvioletred' => '#c71585',
+		'midnightblue' => '#191970',
+		'mintcream' => '#f5fffa',
+		'mistyrose' => '#ffe4e1',
+		'moccasin' => '#ffe4b5',
+		'navajowhite' => '#ffdead',
+		'oldlace' => '#fdf5e6',
+		'olivedrab' => '#6b8e23',
+		'orangered' => '#ff4500',
+		'orchid' => '#da70d6',
+		'palegoldenrod' => '#eee8aa',
+		'palegreen' => '#98fb98',
+		'paleturquoise' => '#afeeee',
+		'palevioletred' => '#db7093',
+		'papayawhip' => '#ffefd5',
+		'peachpuff' => '#ffdab9',
+		'peru' => '#cd853f',
+		'pink' => '#ffc0cb',
+		'plum' => '#dda0dd',
+		'powderblue' => '#b0e0e6',
+		'rosybrown' => '#bc8f8f',
+		'royalblue' => '#4169e1',
+		'saddlebrown' => '#8b4513',
+		'salmon' => '#fa8072',
+		'sandybrown' => '#f4a460',
+		'seagreen' => '#2e8b57',
+		'seashell' => '#fff5ee',
+		'sienna' => '#a0522d',
+		'skyblue' => '#87ceeb',
+		'slateblue' => '#6a5acd',
+		'slategray' => '#708090',
+		'slategrey' => '#708090',
+		'snow' => '#fffafa',
+		'springgreen' => '#00ff7f',
+		'steelblue' => '#4682b4',
+		'tan' => '#d2b48c',
+		'thistle' => '#d8bfd8',
+		'tomato' => '#ff6347',
+		'turquoise' => '#40e0d0',
+		'violet' => '#ee82ee',
+		'wheat' => '#f5deb3',
+		'whitesmoke' => '#f5f5f5',
+		'yellowgreen' => '#9acd32',
+		'rebeccapurple' => '#663399',
+	];
+}

commit dea3b75364bd80214173d9ff5eaa367dd43f773a

docs: correct value contracts
Fixes: 2b7b06719
Fixes: grammm-web-0.1-96-g3a920ff5f
Fixes: grommunio-web-2.8-61-gbf45991cb
Fixes: grommunio-web-2.8-103-g39fdb552a
Fixes: grommunio-web-3.7-51-gc1a1bcc45
Fixes: grommunio-web-3.19-365-g1a69ba704

plugins/files/php/Files/Backend/interface.sharing.php
@@ -68,7 +68,7 @@ interface iFeatureSharing {
 	/**
 	 * Disable sharing for the given files/folders.
 	 *
-	 * @param $patharray Simple array with path's to files or folders
+	 * @param string[] $patharray paths to files or folders
 	 *
 	 * @return bool
 	 */

plugins/files/php/modules/class.fileslistmodule.php
@@ -17,6 +17,7 @@ use Files\Core\AccountStore;
 use Files\Core\Util\Logger as FilesLogger;
 use Files\Core\Util\StringUtil;
 use Phpfastcache\CacheManager;
+use Phpfastcache\Core\Pool\ExtendedCacheItemPoolInterface;
 use Phpfastcache\Drivers\Redis\Config as RedisConfig;
 
 /**
@@ -38,7 +39,7 @@ class FilesListModule extends ListModule {
 	public const ALL_BACKEND_ERR_NOTFOUND = 404;
 
 	/**
-	 * @var phpFastCache cache handler
+	 * @var ExtendedCacheItemPoolInterface cache handler
 	 */
 	public $cache;
 
@@ -70,7 +71,7 @@ class FilesListModule extends ListModule {
 		// Setup the cache
 		$config = new RedisConfig();
 		$config->setHost(PLUGIN_FILES_REDIS_HOST);
-		$config->setPort(PLUGIN_FILES_REDIS_PORT);
+		$config->setPort((int) PLUGIN_FILES_REDIS_PORT);
 		$config->setPassword(PLUGIN_FILES_REDIS_AUTH);
 
 		$this->cache = CacheManager::getInstance('Redis', $config);
@@ -680,7 +681,7 @@ class FilesListModule extends ListModule {
 	 * @param string $displayName display name of the backend or file plugin
 	 * @param string $accountID   Id of the account of the data to cache
 	 *
-	 * @return string version data or null if nothing was found
+	 * @return null|string version data or null if nothing was found
 	 */
 	public function getVersionFromCache($displayName, $accountID = '') {
 		$key = $this->uid . $accountID . $displayName;
@@ -733,7 +734,7 @@ class FilesListModule extends ListModule {
 	 *
 	 * @param string $accountID Id of the account of the data to cache
 	 * @param string $path      Path of the file or folder to create the cache element for
-	 * @param string $data      Data to be cached
+	 * @param array  $data      Data to be cached
 	 */
 	public function setCache($accountID, $path, $data) {
 		$key = $this->makeCacheKey($accountID, $path);
@@ -747,7 +748,7 @@ class FilesListModule extends ListModule {
 	 * @param string $accountID Id of the account of the data to get
 	 * @param string $path      Path of the file or folder to retrieve the cache element for
 	 *
-	 * @return iterable The directory data or null if nothing was found
+	 * @return null|array The directory data or null if nothing was found
 	 */
 	public function getCache($accountID, $path) {
 		$key = $this->makeCacheKey($accountID, $path);

plugins/mdm/php/class.pluginmdmmodule.php
@@ -12,6 +12,7 @@ class PluginMDMModule extends Module {
 	public const FOLDERTYPE = 2;
 	public const FOLDERBACKENDID = 5;
 
+	/** @var null|false|resource */
 	private $stateFolder;
 	private $deviceStates;
 	private $devices;
@@ -312,7 +313,7 @@ class PluginMDMModule extends Module {
 	/**
 	 * Function which is use to gather some statistics about synchronized folders.
 	 *
-	 * @param array $device array of device props
+	 * @param object $device object containing device properties
 	 *
 	 * @return array $syncFoldersProps has list of properties related to synchronized folders
 	 */
@@ -696,7 +697,7 @@ class PluginMDMModule extends Module {
 	 * Returns MAPIFolder object which contains the state information.
 	 * Creates this folder if it is not available yet.
 	 *
-	 * @return MAPIFolder
+	 * @return null|false|resource
 	 */
 	public function getStoreStateFolder() {
 		if (!$this->stateFolder) {

server/includes/core/class.colors.php
@@ -7,9 +7,9 @@ class Colors {
 	/**
 	 * Creates a lighter color.
 	 *
-	 * @param object|string $color      A hexstring or color object (array with r, g, b keys)
-	 * @param float         $percentage the percentage with which the luminance of the color
-	 *                                  should be changed
+	 * @param array|string $color      A hexstring or color array with r, g, b keys
+	 * @param float        $percentage the percentage with which the luminance of the color
+	 *                                 should be changed
 	 *
 	 * @return string A hexstring with the new color (including '#')
 	 */
@@ -31,9 +31,9 @@ class Colors {
 	/**
 	 * Creates a darker color.
 	 *
-	 * @param object|string $color      A hexstring or color object (array with r, g, b keys)
-	 * @param float         $percentage the percentage with which the luminance of the color
-	 *                                  should be changed
+	 * @param array|string $color      A hexstring or color array with r, g, b keys
+	 * @param float        $percentage the percentage with which the luminance of the color
+	 *                                 should be changed
 	 *
 	 * @return string A hexstring with the new color (including '#')
 	 */
@@ -44,9 +44,9 @@ class Colors {
 	/**
 	 * Sets the luminance to a certain value.
 	 *
-	 * @param object|string $color      A hexstring or color object (array with r, g, b keys)
-	 * @param float         $percentage the percentage to which the luminance of the color
-	 *                                  should be set
+	 * @param array|string $color      A hexstring or color array with r, g, b keys
+	 * @param float        $percentage the percentage to which the luminance of the color
+	 *                                 should be set
 	 *
 	 * @return string A hexstring with the new color (including '#')
 	 */
@@ -124,7 +124,7 @@ class Colors {
 	 * See http://www.niwa.nu/2013/05/math-behind-colorspace-conversions-rgb-hsl/ for
 	 * more information.
 	 *
-	 * @param array $color An array with values for r, g, b
+	 * @param array|string $color A hexstring or an array with values for r, g, b
 	 *
 	 * @return array An array with values for h, s, l
 	 */

server/includes/core/class.conversion.php
@@ -294,9 +294,9 @@ class Conversion {
 	 * Convert an JSON restriction structure into a MAPI SRestriction array.
 	 *
 	 * @param array $mapping An associative array mapping property keys to MAPI proptags
-	 * @param array $json    The parsed JSON array data
+	 * @param mixed $json    The parsed JSON restriction data
 	 *
-	 * @return array MAPI restriction array compatible with MAPI extension restriction format
+	 * @return mixed MAPI restriction data compatible with MAPI extension restriction format
 	 */
 	public static function json2restriction($mapping, $json) {
 		if (!is_array($json)) {
@@ -482,9 +482,9 @@ class Conversion {
 	 * Multivalued properties has different property tags in VALUES part
 	 * so we need to find that singlevalued property tag
 	 *
-	 * @param int $propTag The multivalued property tag in string
+	 * @param int|string $propTag The multivalued property tag
 	 *
-	 * @return int The singlevalued property tag
+	 * @return int|string The singlevalued property tag
 	 */
 	public static function convertToSingleValuedProperty($propTag) {
 		if (is_string($propTag)) {

server/includes/download_attachment.php
@@ -923,7 +923,7 @@ class DownloadAttachment extends DownloadBase {
 	 *
 	 * @param string $attachment content fetched from PR_ATTACH_DATA_BIN property of an attachment
 	 *
-	 * @return true if eml is broken, false otherwise
+	 * @return bool true if eml is broken, false otherwise
 	 */
 	public function isBroken($attachment) {
 		// Get header part to process further

server/includes/translations.js.php
@@ -47,8 +47,8 @@ ob_start("ob_gzhandler");
 /**
  * Convert the charset to UTF-8. If the source is an array, convert each item.
  *
- * @param array|string $source  input string or array of strings
- * @param string       $charset original charset of the source string(s)
+ * @param array|false|string $source  input string, array of strings, or false
+ * @param string             $charset original charset of the source string(s)
  *
  * @return array|false|string source data encoded with the new charset
  */

server/test/documentedContractsTest.php
@@ -1,0 +1,23 @@
+<?php
+
+require_once __DIR__ . '/../includes/core/class.colors.php';
+require_once __DIR__ . '/../includes/core/class.conversion.php';
+
+$stringColor = Colors::rgb2hsl('#ffffff');
+$arrayColor = Colors::rgb2hsl(['r' => 255, 'g' => 255, 'b' => 255]);
+
+if ($stringColor !== $arrayColor) {
+	throw new RuntimeException('String and array color inputs must produce the same HSL value');
+}
+
+$restrictionValue = 'unchanged';
+if (Conversion::json2restriction([], $restrictionValue) !== $restrictionValue) {
+	throw new RuntimeException('Scalar restriction data must pass through unchanged');
+}
+
+$propertyTag = 'PR_SUBJECT';
+if (Conversion::convertToSingleValuedProperty($propertyTag) !== $propertyTag) {
+	throw new RuntimeException('Symbolic property tags must pass through unchanged');
+}
+
+echo "Documented contracts test passed\n";