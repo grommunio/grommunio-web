39a8bfdbc08309bf768d36cff9ce5dec4c6750a9
Andreas Lang <andreas.lang@grommunio.com>
2026-09-09 17:50:12 +0000

    client: show the item in the most recent category's colour

--- client/zarafa/calendar/ui/AppointmentView.js
+++ client/zarafa/calendar/ui/AppointmentView.js
@@ -355,7 +355,7 @@ Zarafa.calendar.ui.AppointmentView = Ext.extend(Zarafa.calendar.ui.AbstractDateR
 		// Note: If a label has been set, then it will have been added as the last
 		// category by the getCategories method
 		if ( this.isActive() &&  !Ext.isEmpty(categories) ){
-			return Zarafa.common.categories.Util.getCategoryColor(categories.pop());
+			return Zarafa.common.categories.Util.getCategoryColor(categories.pop(), this.record.get('store_entryid'));
 		}
 
 		// If no category is set then default back to the color scheme of the calendar
